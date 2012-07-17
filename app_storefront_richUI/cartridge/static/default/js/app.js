a75ce8e66620366ae3c03935968c625851ddfd15
Peter Pritchard <ppritchard@demandware.com>
2012-07-17 01:54:29 -0400

    RAP-1850:gift cert stuff

--- app_storefront_richUI/cartridge/static/default/js/app.js
+++ app_storefront_richUI/cartridge/static/default/js/app.js
@@ -1624,14 +1624,70 @@ var app = (function (app, $) {
 
 }(window.app = window.app || {}, jQuery));
 
-// app.giftcard
+//app.giftcert
+(function (app, $) {
+	var $cache;
+
+	function setAddToCartHandler(e) {
+		e.preventDefault();
+		var form = $(this).closest("form");
+		
+		var options = { 
+			url : app.util.ajaxUrl(form.attr('action')),
+			method : 'POST',
+			cache: false,
+			contentType : 'application/json',
+			data : form.serialize()
+		};
+		$.ajax(options).done(function (response) {
+			if( response.success ) {
+				app.ajax.load({
+					url : app.urls.minicartGC,
+					data :{lineItemId : response.result.lineItemId},
+					callback : function(response){
+						app.minicart.show(response);
+						form.find('input,textarea').val('');
+					}
+				});
+			} else {
+				console.log(JSON.stringify(response.errors));
+			}
+		}).fail(function (xhr, textStatus) {
+			// failed
+			if(textStatus === "parsererror") {
+				window.alert(app.resources.BAD_RESPONSE);
+			} else {
+				window.alert(app.resources.SERVER_CONNECTION_ERROR);
+			}
+		});
+	}
+	
+	function initializeCache() {
+		$cache = {
+			addToCart : $("#AddToBasketButton"),
+		};
+	}
+
+	function initializeEvents() {
+		$cache.addToCart.on('click', setAddToCartHandler);
+	}
+		
+	app.giftcert = {
+		init : function(){
+			initializeCache();
+			initializeEvents();
+		}
+	};
+}(window.app = window.app || {}, jQuery));
+
+//app.giftcard
 (function (app, $) {
 	
 	app.giftcard = {
 		checkBalance : function (id, callback) {
 			// load gift certificate details
 			var url = app.util.appendParamToURL(app.urls.giftCardCheckBalance, "giftCertificateID", id);
-
+	
 			app.ajax.getJson({
 				url: url,
 				callback: callback
@@ -2876,6 +2932,8 @@ var app = (function (app, $) {
 	app.account = {
 		init : function () {			
 			initializeEvents();
+			
+			app.giftcert.init();
 		}
 	};
 }(window.app = window.app || {}, jQuery));
