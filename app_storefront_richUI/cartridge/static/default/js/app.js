e9e64ed5fbf5c66bdafa0f967b375eb79bd1bfeb
Alex Clark <aclark@demandware.com>
2013-07-20 17:20:56 -0400

    [RAP-2203 : Store Pickup using Multiple Inventory Lists - code changes  pre-checkout]

--- app_storefront_richUI/cartridge/static/default/js/app.js
+++ app_storefront_richUI/cartridge/static/default/js/app.js
@@ -823,6 +823,9 @@ var app = (function (app, $) {
 			initializeDom();
 			initializeEvents();
 			loadZoom();
+			if(app.enabledStorePickup){
+				app.storeinventory.init();
+			}
 		},
 		readReviews : function(){
 			$('.product-tabs').tabs('select','#tab4');
@@ -3520,6 +3523,9 @@ var app = (function (app, $) {
 			// edit shopping cart line item
 			initializeCache();
 			initializeEvents();
+			if(app.enabledStorePickup){
+				app.storeinventory.init();
+			}
 		}
 	};
 
@@ -4446,6 +4452,348 @@ var app = (function (app, $) {
 		}
 	};
 }(window.app = window.app || {}, jQuery));
+
+/**
+ * @class app.storeinventory
+ */
+(function (app, $) {
+
+	var $cache = {};
+	var pid = null;
+	var currentTemplate = jQuery('#wrapper.pt_cart').length ? "cart" : "pdp";
+	
+	/******* app.storeinventory public object ********/
+	app.storeinventory = {
+		/**
+		 * @function
+		 * @description 
+		 */		
+	 	init : function(){	 		
+			app.storeinventory.initializeCache();
+			app.storeinventory.initializeDom();
+		},		
+	 	
+	 	initializeCache : function () {			
+ 			$cache = {
+ 				preferredStorePanel : jQuery('<div id="preferred-store-panel"/> '),
+ 				storeList : jQuery('<div class="store-list"/>')
+ 			};	
+ 		},
+  		
+  		initializeDom: function(){		
+  			// check for items that trigger dialog
+  			jQuery('#cart-table .set-preferred-store').on('click', function(e){
+  				e.preventDefault();
+ 				app.storeinventory.loadPreferredStorePanel(jQuery(this).parent().attr('id'));
+  			});
+ 
+  			jQuery('body').on('click', '#pdpMain .set-preferred-store', function(e){ 
+ 				e.stopImmediatePropagation();
+  				e.preventDefault();
+ 				app.storeinventory.loadPreferredStorePanel(jQuery(this).parent().attr('id'));
+  			});
+  					
+  			jQuery('.item-delivery-options input.radio-url').click(function(){
+  				app.storeinventory.setLineItemStore(jQuery(this));
+  			});
+  					
+  			if(jQuery(".checkout-shipping").length > 0) app.storeinventory.shippingLoad();
+  					
+ 		},
+ 				
+		setLineItemStore: function(radio) {
+
+			jQuery(radio).parent().parent().children().toggleClass('hide');
+			jQuery(radio).parent().parent().toggleClass('loading');
+
+			app.ajax.getJson({
+				url: app.util.appendParamsToUrl(jQuery(radio).attr('data-url') , {storeid : jQuery(radio).siblings('.storeid').attr('value')}),
+				callback: function(data){
+
+					jQuery(radio).attr('checked','checked');
+					jQuery(radio).parent().parent().toggleClass('loading');	
+					jQuery(radio).parent().parent().children().toggleClass('hide');
+
+				}
+			});
+		},
+
+ 		buildStoreList: function(pid) {
+ 					
+ 			// request results from server
+ 			app.ajax.getJson({
+ 				url: app.util.appendParamsToUrl(app.urls.storesInventory , {pid:pid, zipCode:app.user.zip}),
+ 				callback: function(data){
+ 						
+ 					// clear any previous results, then build new
+ 					$cache.storeList.empty();
+ 					var listings = jQuery("<ul class='store-list'/>");
+ 					if(data && data.length > 0) {					
+ 						for (var i=0; i < 10 && i < data.length; i++) {
+ 							var item=data[i];
+ 								    
+ 							//Disable button if there is no stock for item
+ 							if(item.statusclass == "store-in-stock"){
+						    		var displayButton = '<button value="'+ item.storeId +'" class="button-style-1 select-store-button" data-stock-status="'+item.status+'">' + app.resources.SELECT_STORE + '</button>'; 
+						    	}
+						    	else
+						    	{
+						    		var displayButton = '<button value="'+ item.storeId +'" class="button-style-1 select-store-button" data-stock-status="'+item.status+'" disabled="disabled">' + app.resources.SELECT_STORE + '</button>'; 
+						    	}
+ 																	    
+							// list item for cart
+							if(currentTemplate === 'cart') {
+							
+								listings.append('<li class="store-' +item.storeId + item.status.replace(/ /g,'-') + ' store-tile">' + 
+							    		'<span class="store-tile-address ">' + item.address1 + ',</span>' +
+								    	'<span class="store-tile-city ">' + item.city + '</span>' +
+								    	'<span class="store-tile-state ">' + item.stateCode + '</span>' +
+								    	'<span class="store-tile-postalCode ">' + item.postalCode + '</span>' +
+								    	'<span class="store-tile-status ' + item.statusclass + '">' + item.status + '</span>' +
+								    	displayButton +
+								    	'</li>');							    
+							}
+							    	
+							// list item for pdp
+							else {
+								listings.append('<li class="store-' +item.storeId +' ' + item.status.replace(/ /g,'-') + ' store-tile">' + 
+							    		'<span class="store-tile-address ">' + item.address1 + ',</span>' +
+								    	'<span class="store-tile-city ">' + item.city + '</span>' +
+								    	'<span class="store-tile-state ">' + item.stateCode + '</span>' +
+								    	'<span class="store-tile-postalCode ">' + item.postalCode + '</span>' +
+								    	'<span class="store-tile-status ' + item.statusclass + '">' + item.status + '</span>' +
+								    	displayButton +
+								    	'</li>');
+							}
+ 						}
+ 					}	
+ 							
+ 					// no records
+ 					else {
+ 						if(app.user.zip){
+ 							$cache.storeList.append("<div class='no-results'>No Results</div>");
+ 						}
+ 					}				
+
+ 					// set up pagination for results
+ 					var storeTileWidth = 176;
+ 					var numListings = listings.find('li').size();
+ 					var listingsNav = jQuery('<div id="listings-nav"/>');
+ 					for(var i = 0, link = 1; i <= numListings; i++){
+ 						if(numListings >= i) { listingsNav.append('<a data-index="'+ i +'">'+link+'</a>'); }
+ 					    	link++;
+ 					    	i = i + 2;
+ 					}		    	
+ 					listingsNav.find('a').click(function(){
+ 						jQuery(this).siblings().removeClass('active');
+ 					    	jQuery(this).addClass('active');
+ 					    	jQuery('ul.store-list').animate({'left' : (storeTileWidth * jQuery(this).data('index') * -1) },1000);
+ 					}).first().addClass('active');
+ 					$cache.storeList.after(listingsNav);
+ 					    	
+ 					// check for preferred store id, highlight, move to top
+ 					if(currentTemplate === 'cart'){
+ 					    var selectedButtonText = app.resources.SELECTED_STORE;
+ 					}
+ 					else {
+ 						var selectedButtonText = app.resources.PREFERRED_STORE;
+ 					}
+ 					listings.find('li.store-'+app.user.storeId).addClass('selected').find('button.select-store-button ').text(selectedButtonText);		    	
+
+ 					app.storeinventory.bubbleStoreUp(listings,app.user.storeId);
+ 					    	
+ 					// if there is a block to show results on page (pdp)	    	
+ 					if( currentTemplate !== 'cart' ) {
+ 					    		
+ 						var onPageList = listings.clone();
+ 					    	var thisDiv = jQuery('div#' + pid);
+ 					    		
+ 					    	thisDiv.find('ul.store-list').remove();
+ 					    	thisDiv.append(onPageList);
+
+ 					    	if( onPageList.find('li').size() > 1 ){
+ 					    		thisDiv.find('li:gt(0)').each(function(){
+ 					    			jQuery(this).addClass('extended-list');
+ 					    		});
+ 					    		jQuery('.more-stores').remove();
+ 					    		thisDiv.after('<span class="more-stores">' + app.resources.SEE_MORE + '</span>');
+ 						    	thisDiv.parent().find('.more-stores').click(function(){
+ 						    		if( jQuery(this).text() ===  app.resources.SEE_MORE) {
+ 						    			jQuery(this).text(app.resources.SEE_LESS).addClass('active');
+ 						    		}
+ 						    		else {
+ 						    			jQuery(this).text(app.resources.SEE_MORE).removeClass('active');
+ 						    		}
+ 						    		thisDiv.find(' ul.store-list').toggleClass('expanded');
+ 						    			
+ 						    	});
+ 					    	}		    		
+
+ 					}
+
+ 					// update panel with new list
+ 					listings.width(numListings * storeTileWidth).appendTo($cache.storeList);
+ 					
+ 					// set up 'set preferred store' action on new elements
+ 					listings.find('button.select-store-button').click(function(e){
+ 					
+ 						var selectedStoreId = jQuery(this).val();
+ 					
+ 						if(currentTemplate === 'cart') {
+ 					
+ 							//update selected store and set the lineitem
+ 							var liuuid = jQuery('#preferred-store-panel').find('.srcitem').attr('value');
+ 							jQuery('div[name="'+liuuid+'-sp"] .selected-store-address').html(jQuery(this).siblings('.store-tile-address').text()+' <br />'+jQuery(this).siblings('.store-tile-city').text()+' , '+jQuery(this).siblings('.store-tile-state').text()+' '+jQuery(this).siblings('.store-tile-postalCode').text());
+ 							jQuery('div[name="'+liuuid+'-sp"] .storeid').val(jQuery(this).val());
+ 							jQuery('div[name="'+liuuid+'-sp"] .selected-store-availability').html(jQuery(this).siblings('.store-tile-status'));
+ 							jQuery('div[name="'+liuuid+'-sp"] .radio-url').removeAttr('disabled');
+ 							jQuery('div[name="'+liuuid+'-sp"] .radio-url').click();
+ 							$cache.preferredStorePanel.dialog("close");
+
+ 						}else{
+
+	 						if( app.user.storeId !== selectedStoreId ) {
+
+	 							// set as selected
+	 							app.storeinventory.setPreferredStore(selectedStoreId);
+	 							app.storeinventory.bubbleStoreUp (onPageList, selectedStoreId);	 										
+	 							jQuery('.store-list li.selected').removeClass('selected').find('button.select-store-button').text(app.resources.SELECT_STORE);
+	 							jQuery('.store-list li.store-'+selectedStoreId+' button.select-store-button').text(app.resources.PREFERRED_STORE).parent().addClass('selected');
+	 						}
+
+ 						}
+
+ 					});
+
+ 				} // end ajax callback
+ 			});
+ 		},
+ 		
+ 		bubbleStoreUp : function(list, id) {
+
+ 			var preferredEntry = list.find('li.store-'+id).clone();
+ 			preferredEntry.removeClass('extended-list');
+ 			list.find('.store-tile').not('extended-list').addClass('extended-list');
+ 			list.find('li.store-'+id).remove();
+ 			list.prepend(preferredEntry);
+
+ 		},
+
+ 		loadPreferredStorePanel : function(pid) {
+
+ 			// clear any previous results
+ 			$cache.preferredStorePanel.empty();
+ 			
+ 			// show form if no zip set
+ 				if(app.user.zip === null || app.user.zip === "") {
+ 					$cache.preferredStorePanel
+ 						.append('<div><input type="text" id="userZip" placeholder="' + app.resources.ENTER_ZIP + '"/><button id="set-user-zip" class="button-style-1">' + app.resources.SEARCH + '</button></div>')
+ 							.find('#set-user-zip')
+ 								.click(function(){
+ 									var zipCodePattern = /^\d{5}$/;
+ 									var enteredZip = jQuery('#userZip').val();
+ 									if( zipCodePattern.test(enteredZip) ) {
+ 										//good zip
+ 										app.storeinventory.setUserZip(enteredZip);
+ 										app.storeinventory.loadPreferredStorePanel(pid);
+ 									}
+ 									else {
+ 										//bad zip
+ 									}
+ 								});
+ 					$cache
+ 						.preferredStorePanel
+ 							.find('#userZip')
+ 								.keypress(function(e) {
+ 									code = e.keyCode ? e.keyCode : e.which;
+ 									if(code.toString() == 13) {
+ 										$cache.preferredStorePanel.find('#set-user-zip').trigger('click');
+ 									}
+ 					});
+
+ 					// clear any on-page results
+ 					jQuery('div.store-stock ul.store-list').remove();
+ 
+ 				}
+ 				// zip is set, build list
+ 				else {
+ 					app.storeinventory.buildStoreList(pid);
+ 					$cache
+ 						.preferredStorePanel
+ 						.append("<div>For " + app.user.zip + " <span class='update-location'>" + app.resources.CHANGE_LOCATION + "</span></div>" )
+ 						.append($cache.storeList);
+ 					$cache
+ 						.preferredStorePanel
+ 							.find('span.update-location')
+ 								.click(function(){
+ 									app.storeinventory.setUserZip(null);
+ 									app.storeinventory.loadPreferredStorePanel(pid);
+ 					});
+
+ 				}
+ 					
+ 				// append close button for pdp
+ 				if(currentTemplate !== "cart") {
+	 				if(app.user.storeId !== null) {
+	 					$cache.preferredStorePanel.append("<button class='close button-style-1'>" + app.resources.CONTINUE_WITH_STORE + "</button>");
+	 				}
+	 				else if(app.user.zip !== null) {
+	 					$cache.preferredStorePanel.append("<button class='close button-style-1'>" + app.resources.CONTINUE + "</button>");
+	 				}
+ 				}else{
+ 					$cache.preferredStorePanel.append("<input type='hidden' class='srcitem' value='" + pid + "'>");
+ 				}
+ 					
+ 				// open the dialog
+ 				$cache.preferredStorePanel.dialog({
+ 					width: 550,
+ 					modal: true,
+ 					title: app.resources.STORE_NEAR_YOU
+ 				});
+
+ 				// action for close/continue
+ 				jQuery('button.close').click(function(){
+ 					$cache.preferredStorePanel.dialog("close");
+ 				});
+
+ 		},
+ 		
+ 		setUserZip : function(zip) {
+
+ 			app.user.zip = zip;
+ 			jQuery.ajax({
+ 				type: "POST",
+ 				url: app.urls.setZipCode,
+ 				data: { zipCode : zip }
+ 			}).fail(function() {
+
+ 			});
+
+ 		},
+ 		
+ 		setPreferredStore : function(id) {
+ 		
+ 			app.user.storeId = id;
+ 			jQuery.post(app.urls.setPreferredStore, { storeId : id }, function(data) {
+ 				jQuery('.selected-store-availability').html(data);
+ 			});
+ 			
+ 		},
+ 		
+ 		shippingLoad : function() {
+ 			$cache.checkoutForm = jQuery("form.address");
+ 			$cache.checkoutForm.off("click");
+ 			$cache.checkoutForm.on("click", ".is-gift-yes, .is-gift-no", function (e) {
+ 				jQuery(this).parent().siblings(".gift-message-text").toggle(jQuery(this).checked);
+ 			});
+ 			return null;
+ 		}
+
+	};
+}(window.app = window.app || {}, jQuery));
+
+
+
 
 // jquery extensions
 (function ($) {
