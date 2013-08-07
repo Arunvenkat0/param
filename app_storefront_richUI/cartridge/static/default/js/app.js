9d07fb826ad23a2e523d4a4ae4d73b45b84b29af
Kash Khan <kkhan@demandware.com>
2013-08-07 21:44:38 +0100

    RAP-2461 Back/next navigation for quickview layer

--- app_storefront_richUI/cartridge/static/default/js/app.js
+++ app_storefront_richUI/cartridge/static/default/js/app.js
@@ -2676,6 +2676,51 @@ var app = (function (app, $) {
 			$cache.quickView = $("<div/>").attr("id", "#QuickViewDialog").appendTo(document.body);
 			return $cache.quickView;
 		},
+		
+		initializeQuickViewNav : function(qvUrl) {
+			qvUrl = qvUrl.substring(0, qvUrl.indexOf('?'));
+
+			this.searchesultsContainer = $('#search-result-items').parent();
+			this.productLinks = this.searchesultsContainer.find('.thumb-link');
+
+			this.btnNext = $('.quickview-next');
+			this.btnPrev = $('.quickview-prev');
+
+			this.btnNext.click(this.navigateQuickview.bind(this));
+			this.btnPrev.click(this.navigateQuickview.bind(this));
+
+			for ( var i = 0; i < this.productLinks.length; i++) {
+				if (qvUrl == this.productLinks[i]) {
+					this.productLinkIndex = i;
+				}
+			}
+
+			if (this.productLinkIndex == this.productLinks.length - 1) {
+				this.btnNext.hide();
+			}
+
+			if (this.productLinkIndex == 0) {
+				this.btnPrev.hide();
+			}
+		},
+
+		navigateQuickview : function(event) {
+			var button = $(event.currentTarget);
+
+			if (button.hasClass('quickview-next')) {
+				this.productLinkIndex++;
+			} else {
+				this.productLinkIndex--;
+			}
+
+			app.quickView.show({
+				url : this.productLinks[this.productLinkIndex].href,
+				source : 'quickview'
+			});
+
+			event.preventDefault();
+		},
+		
 		// show quick view dialog and send request to the server to get the product
 		// options.source - source of the dialog i.e. search/cart
 		// options.url - product url
@@ -2702,6 +2747,8 @@ var app = (function (app, $) {
 					}
 				});
 				$cache.quickView.dialog('open');
+				
+				app.quickView.initializeQuickViewNav(this.url);
 			};
 			app.product.get(options);
 
