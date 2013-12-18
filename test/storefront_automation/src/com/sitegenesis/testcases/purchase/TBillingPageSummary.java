b415ca61d6087166b43995da02a4451c1711a392
Jeff Bogner <jbogner@demandware.com>
2013-12-18 15:35:12 -0500

    Adding more test updates to use project WebDriver settings.

--- test/storefront_automation/src/com/sitegenesis/testcases/purchase/TBillingPageSummary.java
+++ test/storefront_automation/src/com/sitegenesis/testcases/purchase/TBillingPageSummary.java
@@ -2,7 +2,7 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package com.sitegenesis.testcases.purchase;
-import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.sitegenesis.util.AbstractBrowserScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
@@ -18,6 +18,6 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  */
 @ScriptName
 ("testcases.purchase.TBillingPageSummary")
-public class TBillingPageSummary extends AbstractScriptTestCase
+public class TBillingPageSummary extends AbstractBrowserScriptTestCase
 {
 }
