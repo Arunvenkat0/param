9e12e9f25d15ada3c4a8d813fa37d06e6d363a37
Jeff Bogner <jbogner@demandware.com>
2014-01-13 17:23:05 -0500

    Mass migration of tests into the automation smoke test suite to harmonize with the Test Rail Smoke Test suite.

---
+++ test/storefront_automation/src/com/sitegenesis/testcases/smoketest/catalog/TCheckItemsPerPage.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package com.sitegenesis.testcases.smoketest.catalog;
+import com.sitegenesis.util.AbstractBrowserScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Opens a category page and changes the number of items to be displayed. Validates the correctness of the result.
+ */
+@ScriptName
+("testcases.smoketest.catalog.TCheckItemsPerPage")
+public class TCheckItemsPerPage extends AbstractBrowserScriptTestCase
+{
+}
---
+++ test/storefront_automation/src/com/sitegenesis/testcases/smoketest/wishlist/TSearchProductAndAddToWishlist.java
@@ -0,0 +1,20 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package com.sitegenesis.testcases.smoketest.wishlist;
+import com.sitegenesis.util.AbstractBrowserScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Test for searching a product an adding it to a wishlist.
+ - search for a product
+ - add to wishlist
+ - register new user
+ - validate product data on the wishlist
+ */
+@ScriptName
+("testcases.smoketest.wishlist.TSearchProductAndAddToWishlist")
+public class TSearchProductAndAddToWishlist extends AbstractBrowserScriptTestCase
+{
+}
