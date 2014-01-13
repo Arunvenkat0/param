df90b68f229bbf668bc8024c69101f8656988c8f
Jeff Bogner <jbogner@demandware.com>
2014-01-13 18:16:59 -0500

    Another cleanup of saved files.

---
+++ test/storefront_automation/src/testcases/multipleshipment/TMultiShipmentInternational.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package testcases.multipleshipment;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("testcases.multipleshipment.TMultiShipmentInternational")
+public class TMultiShipmentInternational extends AbstractScriptTestCase
+{
+}
