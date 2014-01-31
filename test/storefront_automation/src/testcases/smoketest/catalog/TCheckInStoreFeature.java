b33ebeb42b1563e5b78fefe1d712f0feafb01044
Jeff Bogner <jbogner@demandware.com>
2014-01-31 15:17:28 -0500

    Updates for 14.2 and stability enhancements.

---
+++ test/storefront_automation/src/testcases/smoketest/catalog/TCheckInStoreFeature.java
@@ -0,0 +1,27 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package testcases.smoketest.catalog;
+import org.junit.Ignore;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * DISABLED:  Requires "In Store" configuration.
+ 
+ Checks the InStore feature at the PDP.
+ Validates the search field including the error handling.
+ - searches for a Store
+ - pages through the store results
+ - selects a store
+ - changes the location
+ - selects another store
+ - adds product to cart and verifies that the selected store is set at the cart page
+ */
+@ScriptName
+("testcases.smoketest.catalog.TCheckInStoreFeature")
+@Ignore
+public class TCheckInStoreFeature extends AbstractScriptTestCase
+{
+}
