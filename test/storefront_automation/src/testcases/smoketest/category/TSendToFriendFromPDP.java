4afc68cfc457fe39411b6be1f02cdb83480a21da
Jeff Bogner <jbogner@demandware.com>
2014-02-12 16:13:12 -0500

    more chrome updates

--- test/storefront_automation/src/testcases/smoketest/category/TSendToFriendFromPDP.java
+++ test/storefront_automation/src/testcases/smoketest/category/TSendToFriendFromPDP.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID: (C84770,C84745,C84746) (C84745)  Smoke Test » Send to a Friend » Send to a Friend Overlay (C84746) Smoke Test » Send to a Friend » Send to a Friend Review 
+ * Test Case ID: (C84770,C84745,C84746) (C84745)  Smoke Test » Send to a Friend » Send to a Friend Overlay (C84746) Smoke Test » Send to a Friend » Send to a Friend Review
  */
 @ScriptName
 ("testcases.smoketest.category.TSendToFriendFromPDP")
--- test/storefront_automation/src/testcases/smoketest/homepage/THomepageGeneral.java
+++ test/storefront_automation/src/testcases/smoketest/homepage/THomepageGeneral.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C84757,C84776)  (C84757)  Smoke Test » Homepage » Homepage, General  (C84776) Smoke Test » Header | Global Navigation | Footer » Footer 
+ * Test Case ID:(C84757,C84776)  (C84757)  Smoke Test » Homepage » Homepage, General  (C84776) Smoke Test » Header | Global Navigation | Footer » Footer
  */
 @ScriptName
 ("testcases.smoketest.homepage.THomepageGeneral")
