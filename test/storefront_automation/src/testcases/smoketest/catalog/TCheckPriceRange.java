4e0d97cdc8dd889f6209bfebb95b534b772e52eb
Jeff Bogner <jbogner@demandware.com>
2014-02-20 15:07:20 -0500

    Many function updates and fixes for Case Insensitive text

--- test/storefront_automation/src/testcases/smoketest/catalog/TCheckPriceRange.java
+++ test/storefront_automation/src/testcases/smoketest/catalog/TCheckPriceRange.java
@@ -9,7 +9,6 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 /**
  * [Test Case ID:(C85028 partial ) ] Smoke Test » Category Landing Page » Price Range  
  Checks the correct behaviour of the price range. Therefore a product price is taken from the grid page and validated against the price at the quickview and the product detail page.
- 
  */
 @ScriptName
 ("testcases.smoketest.catalog.TCheckPriceRange")
