b9059f8a3d98eea4116a8973cb6e51b24d7c9b04
Jeff Bogner <jbogner@demandware.com>
2014-02-21 18:57:20 -0500

    Refactoring with library functions.

--- test/storefront_automation/src/testcases/smoketest/productgrid/TStandardProduct.java
+++ test/storefront_automation/src/testcases/smoketest/productgrid/TStandardProduct.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C84681) Smoke Test » Search Results » Product Grid » Product Grid Display: Pricing 
+ * Test Case ID:(C130249) Smoke Test » Search Results » Product Grid » Product Grid Display: Standard Product Pricing 
  */
 @ScriptName
 ("testcases.smoketest.productgrid.TStandardProduct")
