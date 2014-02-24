4640bd64bea1a782d94a42d7c7d3e3f1d6245bb0
Jeff Bogner <jbogner@demandware.com>
2014-02-24 18:51:54 -0500

    Mega refactorting deletions

--- test/storefront_automation/src/testcases/smoketest/wishlist/TOrderFromOwnWishlist.java
+++ test/storefront_automation/src/testcases/smoketest/wishlist/TOrderFromOwnWishlist.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C85036) Smoke Test » My Account - Returning Customer » Wish List » Order From Own Wishlist 
+ * Test Case ID:(C85036) Smoke Test » My Account - Returning Customer » Wish List » Order From Own Wishlist
  */
 @ScriptName
 ("testcases.smoketest.wishlist.TOrderFromOwnWishlist")
