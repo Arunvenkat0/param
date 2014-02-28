36766f12c455584769f5acbc09fcddfd45a5e47c
Jeff Bogner <jbogner@demandware.com>
2014-02-28 15:28:43 -0500

    added comments to a single test.

--- test/storefront_automation/src/testcases/smoketest/catalog/TCheckCatalogNavigation.java
+++ test/storefront_automation/src/testcases/smoketest/catalog/TCheckCatalogNavigation.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C84713)  Smoke Test » Product Details » PDP: Previous and Next Navigation 
+ * Test Case ID:(C84713)  Smoke Test » Product Details » PDP: Previous and Next Navigation
  */
 @ScriptName
 ("testcases.smoketest.catalog.TCheckCatalogNavigation")
