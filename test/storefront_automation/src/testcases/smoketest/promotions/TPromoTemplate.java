14a8844ed911ebe5b97a18c5910e255486708352
Jeff Bogner <jbogner@demandware.com>
2014-03-20 18:18:40 -0400

    More Promotions test work

---
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromoTemplate.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package testcases.smoketest.promotions;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("testcases.smoketest.promotions.TPromoTemplate")
+public class TPromoTemplate extends AbstractScriptTestCase
+{
+}
