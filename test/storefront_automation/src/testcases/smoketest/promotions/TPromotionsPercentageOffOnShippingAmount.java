67745fd131f8da7e4a65e162a603688ea6354e1e
Jeff Bogner <jbogner@demandware.com>
2014-03-24 14:59:27 -0400

    Test update, Promotions xml import added to testdata, clean up of re-names.

--- test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsPercentageOffOnShippingAmount.java
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsPercentageOffOnShippingAmount.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C130247)  
+ * Test Case ID:(C130247)
  */
 @ScriptName
 ("testcases.smoketest.promotions.TPromotionsPercentageOffOnShippingAmount")
