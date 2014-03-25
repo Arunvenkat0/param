e03ba35e65eeaaf356c1ca38ca769ba2c00f351a
Jeff Bogner <jbogner@demandware.com>
2014-03-25 14:01:36 -0400

    Clean up work files.

--- test/storefront_automation/src/testcases/smoketest/promotions/TPromotions_Template.java
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotions_Template.java
@@ -2,15 +2,17 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.promotions;
+import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:()  
+ * Test Case ID:()
  */
 @ScriptName
 ("testcases.smoketest.promotions.TPromotions_Template")
+@Ignore
 public class TPromotions_Template extends AbstractScriptTestCase
 {
 }
