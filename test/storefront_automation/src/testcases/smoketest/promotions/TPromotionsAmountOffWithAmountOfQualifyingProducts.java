bac732e9952646e92a18f1ce3e404b93635b98e6
Jeff Bogner <jbogner@demandware.com>
2014-03-26 13:22:47 -0400

    Further test development for Promotions.

--- test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsAmountOffWithAmountOfQualifyingProducts.java
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsAmountOffWithAmountOfQualifyingProducts.java
@@ -2,6 +2,7 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.promotions;
+import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
@@ -11,6 +12,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  */
 @ScriptName
 ("testcases.smoketest.promotions.TPromotionsAmountOffWithAmountOfQualifyingProducts")
+@Ignore
 public class TPromotionsAmountOffWithAmountOfQualifyingProducts extends AbstractScriptTestCase
 {
 }
