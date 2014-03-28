853ffcb1e97365cad48f1f425e0b3e972ecdf7b1
Jeff Bogner <jbogner@demandware.com>
2014-03-28 15:58:36 -0400

    More promotions test work.

--- test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsFixedPriceForProducts.java
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsFixedPriceForProducts.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C130268)
+ * Test Case ID:(C130270)
  */
 @ScriptName
 ("testcases.smoketest.promotions.TPromotionsFixedPriceForProducts")
