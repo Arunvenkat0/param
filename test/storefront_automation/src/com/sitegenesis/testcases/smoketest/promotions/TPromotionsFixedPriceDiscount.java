158a8b37b0d347ecbbe3335731923ce5c94bc8e6
Jeff Bogner <jbogner@demandware.com>
2014-03-31 14:57:10 -0400

    Harmonized promotions tests to JUnit/WebDriver compatibility.

---
+++ test/storefront_automation/src/com/sitegenesis/testcases/smoketest/promotions/TPromotionsFixedPriceDiscount.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package com.sitegenesis.testcases.smoketest.promotions;
+import com.sitegenesis.util.AbstractBrowserScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Test Case ID:(C130269)
+ */
+@ScriptName
+("testcases.smoketest.promotions.TPromotionsFixedPriceDiscount")
+public class TPromotionsFixedPriceDiscount extends AbstractBrowserScriptTestCase
+{
+}
