e7376e35d9de8c819b4a79b304dcf8c4de6a8a30
Jeff Bogner <jbogner@demandware.com>
2014-03-31 18:01:39 -0400

    Clean up promotion files and data sheets.

--- test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100.java
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100.java
@@ -2,6 +2,7 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.promotions;
+import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
@@ -11,6 +12,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  */
 @ScriptName
 ("testcases.smoketest.promotions.TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100")
+@Ignore
 public class TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100 extends AbstractScriptTestCase
 {
 }
