250092d19b9f7a8b977778829b2e85fe3d0dd93f
Jeff Bogner <jbogner@demandware.com>
2014-04-03 09:58:30 -0400

    Finishing PromotionsAlertApproachingPromotion test.

--- test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsAlertApproachingPromotion.java
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsAlertApproachingPromotion.java
@@ -2,17 +2,15 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.promotions;
-import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * BUG Test Case ID:(C130264)
+ * Test Case ID:(C130264)
  */
 @ScriptName
 ("testcases.smoketest.promotions.TPromotionsAlertApproachingPromotion")
-@Ignore
 public class TPromotionsAlertApproachingPromotion extends AbstractScriptTestCase
 {
 }
