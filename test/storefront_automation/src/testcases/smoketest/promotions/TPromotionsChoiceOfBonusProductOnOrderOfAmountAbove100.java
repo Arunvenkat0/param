5112f9d5e5dab899e93515a2e063120fd0b1122c
Jeff Bogner <jbogner@demandware.com>
2014-04-11 13:55:13 -0400

    SG Model Organization Refactoring and preparation for auto build execution.

--- test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100.java
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100.java
@@ -2,15 +2,18 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.promotions;
+import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * BUG Test Case ID:(C130263)
+ * BUG: 'RAP-2172'
+ Test Case ID:(C130263)
  */
 @ScriptName
 ("testcases.smoketest.promotions.TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100")
+@Ignore
 public class TPromotionsChoiceOfBonusProductOnOrderOfAmountAbove100 extends AbstractScriptTestCase
 {
 }
