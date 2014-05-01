eef6d5b21c1df685ec02b27ccecb88721f1551f6
Jeff Bogner <jbogner@demandware.com>
2014-05-01 11:38:32 -0400

    New promottions tests disabled as incomplete.

---
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotions_A_ExclusivityClassAndExclusivityGlobal.java
@@ -0,0 +1,18 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package testcases.smoketest.promotions;
+import org.junit.Ignore;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Test Case ID:(C130270)
+ */
+@ScriptName
+("testcases.smoketest.promotions.TPromotions_A_ExclusivityClassAndExclusivityGlobal")
+@Ignore
+public class TPromotions_A_ExclusivityClassAndExclusivityGlobal extends AbstractScriptTestCase
+{
+}
---
+++ test/storefront_automation/src/testcases/smoketest/promotions/TPromotions_A_ExclusivityNoAndExclusivityClass.java
@@ -0,0 +1,18 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package testcases.smoketest.promotions;
+import org.junit.Ignore;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Test Case ID:(C130270)
+ */
+@ScriptName
+("testcases.smoketest.promotions.TPromotions_A_ExclusivityNoAndExclusivityClass")
+@Ignore
+public class TPromotions_A_ExclusivityNoAndExclusivityClass extends AbstractScriptTestCase
+{
+}
