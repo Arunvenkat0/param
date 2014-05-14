0ea5372d6285db90d8f931506a15f122e0e575f3
Jeff Bogner <jbogner@demandware.com>
2014-05-14 12:16:24 -0400

    Created JUnit wrappers for new Promotions test cases.

---
+++ test/storefront_automation/src/com/sitegenesis/testcases/smoketest/promotions/TPromotionsExclusivityNoAndExclusivityClass.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package com.sitegenesis.testcases.smoketest.promotions;
+import com.sitegenesis.util.AbstractBrowserScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Test Case ID:(C131702)
+ */
+@ScriptName
+("testcases.smoketest.promotions.TPromotionsExclusivityNoAndExclusivityClass")
+public class TPromotionsExclusivityNoAndExclusivityClass extends AbstractBrowserScriptTestCase
+{
+}
