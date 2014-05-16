c968e83faaaa2fdb0aae622a325289400dfff245
Jeff Bogner <jbogner@demandware.com>
2014-05-16 12:02:20 -0400

    Promotions test updates, initial multi-inventory work, refactoring.

--- test/storefront_automation/src/testcases/smoketest/multipleinventory/Template.java
+++ test/storefront_automation/src/testcases/smoketest/multipleinventory/Template.java
@@ -2,6 +2,7 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.multipleinventory;
+import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
@@ -11,6 +12,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  */
 @ScriptName
 ("testcases.smoketest.multipleinventory.Template")
+@Ignore
 public class Template extends AbstractScriptTestCase
 {
 }
