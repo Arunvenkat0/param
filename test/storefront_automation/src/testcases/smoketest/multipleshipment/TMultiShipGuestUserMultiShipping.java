bf0f7537e19861a32e891d16a016e50be31c0acf
Jeff Bogner <jbogner@demandware.com>
2014-05-22 17:56:21 -0400

    Mostly new files for multi-shipping tests.

---
+++ test/storefront_automation/src/testcases/smoketest/multipleshipment/TMultiShipGuestUserMultiShipping.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package testcases.smoketest.multipleshipment;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("testcases.smoketest.multipleshipment.TMultiShipGuestUserMultiShipping")
+public class TMultiShipGuestUserMultiShipping extends AbstractScriptTestCase
+{
+}
