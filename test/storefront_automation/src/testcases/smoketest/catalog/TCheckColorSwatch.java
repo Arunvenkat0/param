947651eae294ba36d2d3725c9fe0123197a34777
Jeff Bogner <jbogner@demandware.com>
2014-05-22 10:16:36 -0400

    Disabled TCheckColorSwatch that has bad code.

--- test/storefront_automation/src/testcases/smoketest/catalog/TCheckColorSwatch.java
+++ test/storefront_automation/src/testcases/smoketest/catalog/TCheckColorSwatch.java
@@ -2,6 +2,7 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.catalog;
+import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
@@ -11,6 +12,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  */
 @ScriptName
 ("testcases.smoketest.catalog.TCheckColorSwatch")
+@Ignore
 public class TCheckColorSwatch extends AbstractScriptTestCase
 {
 }
