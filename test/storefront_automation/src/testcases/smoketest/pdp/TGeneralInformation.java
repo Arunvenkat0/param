dd55ea35bd8b969d564ccb0f8c20b13b5a2eedd0
Jeff Bogner <jbogner@demandware.com>
2014-05-22 13:52:35 -0400

    Test tweaks and commenting out current bug ticketed tests.

--- test/storefront_automation/src/testcases/smoketest/pdp/TGeneralInformation.java
+++ test/storefront_automation/src/testcases/smoketest/pdp/TGeneralInformation.java
@@ -2,15 +2,17 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.pdp;
+import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * DISABLED This test requires a logged in session, needs to be updated. ' Test Case ID:(C84710) Smoke Test » Product Details » PDP: General Information
+ * BUG: RAP-???? Test Case ID:(C84710) Smoke Test » Product Details » PDP: General Information
  */
 @ScriptName
 ("testcases.smoketest.pdp.TGeneralInformation")
+@Ignore
 public class TGeneralInformation extends AbstractScriptTestCase
 {
 }
