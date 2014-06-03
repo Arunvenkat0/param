fb402fd6243e53a4b1530ae428810335170da644
Jeff Bogner <jbogner@demandware.com>
2014-06-03 17:07:16 -0400

    Updated notes.

--- test/storefront_automation/src/testcases/smoketest/multipleshipment/TMultiShipWithDifferentShippingMethods.java
+++ test/storefront_automation/src/testcases/smoketest/multipleshipment/TMultiShipWithDifferentShippingMethods.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C132192)
+ * Test Case ID:(C132195)
  */
 @ScriptName
 ("testcases.smoketest.multipleshipment.TMultiShipWithDifferentShippingMethods")
--- test/storefront_automation/src/testcases/smoketest/multipleshipment/TMultiShipWithShippingPromotion.java
+++ test/storefront_automation/src/testcases/smoketest/multipleshipment/TMultiShipWithShippingPromotion.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C132192)
+ * Test Case ID:(C132194)
  */
 @ScriptName
 ("testcases.smoketest.multipleshipment.TMultiShipWithShippingPromotion")
