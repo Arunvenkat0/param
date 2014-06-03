84413fdd7cba7baaede1f81442cf9bc051434e33
Jeff Bogner <jbogner@demandware.com>
2014-06-03 18:04:01 -0400

    Recovery of catalog.TCheckColorSwatch test.

--- test/storefront_automation/src/testcases/smoketest/catalog/TCheckColorSwatch.java
+++ test/storefront_automation/src/testcases/smoketest/catalog/TCheckColorSwatch.java
@@ -2,18 +2,16 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package testcases.smoketest.catalog;
-import org.junit.Ignore;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * DISABLED: 'RAP-2406'
+ * DISABLED: 
  selected color not persisting. Test Case ID:(C84682) Smoke Test » Search Results » Product Grid » Product Grid Display: Images and Swatches
  */
 @ScriptName
 ("testcases.smoketest.catalog.TCheckColorSwatch")
-@Ignore
 public class TCheckColorSwatch extends AbstractScriptTestCase
 {
 }
