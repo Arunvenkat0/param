fef7e2a0c6970bf636eabef5e8ef46763944452b
Tri Nguyen <tri@tridnguyen.com>
2014-07-09 10:24:26 -0400

    add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+	gulp.src('app_storefront_core/cartridge/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app_storefront_core/cartridge/static/default/css'));
+});
