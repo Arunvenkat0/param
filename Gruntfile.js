01f32992ff23c38b832076897dc7541f0eb3fbd2
Tri Nguyen <tri@tridnguyen.com>
2014-07-11 10:02:49 -0400

    simple grunt sass watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,32 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		watch: {
 			sass: {
-				
+				files: ['app_storefront_core/cartridge/scss/*.scss'],
+				tasks: ['sass', 'autoprefixer']
 			}
-		}
+		},
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					sourcemap: true
+				},
+				files: {
+					'app_storefront_core/cartridge/static/default/css/style.css': 'app_storefront_core/cartridge/scss/style.scss'
+				}
+			}
+		},
+		autoprefixer: {
+			dev: {
+				src: 'app_storefront_core/cartridge/static/default/css/style.css',
+				dest: 'app_storefront_core/cartridge/static/default/css/style.css'
+			}
+		},
 	});
+
+	grunt.registerTask('default', [
+		'sass',
+		'autoprefixer',
+		'watch'
+	]);
 }
