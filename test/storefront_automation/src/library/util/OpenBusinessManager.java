5e52327c5ab34547c1e51a0130946520b4331bde
Jeff Bogner <jbogner@demandware.com>
2014-07-17 16:04:16 -0400

    More changes for 14.8 and a new utility.

--- test/storefront_automation/src/library/util/OpenBusinessManager.java
+++ test/storefront_automation/src/library/util/OpenBusinessManager.java
@@ -1,16 +1,16 @@
 /*
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
-package testcases.smoketest.purchase;
+package library.util;
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * Test Case ID:(C84728)
+ * 
  */
 @ScriptName
-("testcases.smoketest.purchase.TPurchaseGiftCertificates")
-public class TPurchaseGiftCertificates extends AbstractScriptTestCase
+("library.util.OpenBusinessManager")
+public class OpenBusinessManager extends AbstractScriptTestCase
 {
 }
