4e931036ecc52dd44f8e849dc3527251af358364
Tri Nguyen <tri@tridnguyen.com>
2014-07-24 17:41:05 -0400

    commonjs cart

--- app_storefront_richUI/cartridge/javascripts/cart.js
+++ app_storefront_richUI/cartridge/javascripts/cart.js
@@ -1,104 +1,108 @@
+'use strict';
+
+var account = require('./account'),
+	bonusProductsView = require('./bonus-products-view'),
+	page = require('./page'),
+	quickview = require('./quickview'),
+	storeinventory = require('./storeinventory'),
+	util = require('./util')
 /**
- * @class app.cart
+ * @private
+ * @function
+ * @description Updates the cart with new data
+ * @param {Object} postdata An Object representing the the new or uptodate data
+ * @param {Object} A callback function to be called
  */
-(function (app, $) {
-	var $cache = {};
+function updateCart(postdata, callback) {
+	var url = util.ajaxUrl(app.urls.addProduct);
+	$.post(url, postdata, callback || cart.refresh);
+}
+/**
+ * @private
+ * @function
+ * @description Cache initialization of the cart page
+ */
+function initializeCache() {
+	$cache = {
+		cartTable : $("#cart-table"),
+		itemsForm : $("#cart-items-form"),
+		addCoupon : $("#add-coupon"),
+		couponCode : $("form input[name$='_couponCode']")
+	};
+}
+/**
+ * @private
+ * @function
+ * @description Binds events to the cart page (edit item's details, bonus item's actions, coupon code entry )
+ */
+function initializeEvents() {
+	$cache.cartTable.on("click", ".item-edit-details a", function (e) {
+		e.preventDefault();
+		quickView.show({
+			url : e.target.href,
+			source : "cart"
+		});
+	})
+	.on("click", ".bonus-item-actions a", function (e) {
+		e.preventDefault();
+		bonusProductsView.show(this.href);
+	});
+
+	// override enter key for coupon code entry
+	$cache.couponCode.on("keydown", function (e) {
+		if (e.which === 13 && $(this).val().length===0) { return false; }
+	});
+}
+
+/******* cart public object ********/
+var cart = {
 	/**
-	 * @private
+	 * @function
+	 * @description Adds new item to the cart
+	 * @param {Object} postdata An Object representing the the new or uptodate data
+	 * @param {Object} A callback function to be called
+	 */
+	add : function (postdata, callback) {
+		updateCart(postdata, callback);
+	},
+	/**
+	 * @function
+	 * @description Hook for removing item from the cart
+	 *
+	 */
+	remove : function () {
+		return;
+	},
+	/**
 	 * @function
 	 * @description Updates the cart with new data
 	 * @param {Object} postdata An Object representing the the new or uptodate data
 	 * @param {Object} A callback function to be called
 	 */
-	function updateCart(postdata, callback) {
-		var url = app.util.ajaxUrl(app.urls.addProduct);
-		$.post(url, postdata, callback || app.cart.refresh);
-	}
+	update : function (postdata, callback) {
+		updateCart(postdata, callback);
+	},
 	/**
-	 * @private
 	 * @function
-	 * @description Cache initialization of the cart page
+	 * @description Refreshes the cart without posting
 	 */
-	function initializeCache() {
-		$cache = {
-			cartTable : $("#cart-table"),
-			itemsForm : $("#cart-items-form"),
-			addCoupon : $("#add-coupon"),
-			couponCode : $("form input[name$='_couponCode']")
-		};
-	}
+	refresh : function () {
+		// refresh without posting
+		page.refresh();
+	},
 	/**
-	 * @private
 	 * @function
-	 * @description Binds events to the cart page (edit item's details, bonus item's actions, coupon code entry )
+	 * @description Initializes the functionality on the cart
 	 */
-	function initializeEvents() {
-		$cache.cartTable.on("click", ".item-edit-details a", function (e) {
-			e.preventDefault();
-			app.quickView.show({
-				url : e.target.href,
-				source : "cart"
-			});
-		})
-		.on("click", ".bonus-item-actions a", function (e) {
-			e.preventDefault();
-			app.bonusProductsView.show(this.href);
-		});
-
-		// override enter key for coupon code entry
-		$cache.couponCode.on("keydown", function (e) {
-			if (e.which === 13 && $(this).val().length===0) { return false; }
-		});
-	}
-	/******* app.cart public object ********/
-	app.cart = {
-		/**
-		 * @function
-		 * @description Adds new item to the cart
-		 * @param {Object} postdata An Object representing the the new or uptodate data
-		 * @param {Object} A callback function to be called
-		 */
-		add : function (postdata, callback) {
-			updateCart(postdata, callback);
-		},
-		/**
-		 * @function
-		 * @description Hook for removing item from the cart
-		 *
-		 */
-		remove : function () {
-			return;
-		},
-		/**
-		 * @function
-		 * @description Updates the cart with new data
-		 * @param {Object} postdata An Object representing the the new or uptodate data
-		 * @param {Object} A callback function to be called
-		 */
-		update : function (postdata, callback) {
-			updateCart(postdata, callback);
-		},
-		/**
-		 * @function
-		 * @description Refreshes the cart without posting
-		 */
-		refresh : function () {
-			// refresh without posting
-			app.page.refresh();
-		},
-		/**
-		 * @function
-		 * @description Initializes the functionality on the cart
-		 */
-		init : function () {
-			// edit shopping cart line item
-			initializeCache();
-			initializeEvents();
-			if(app.enabledStorePickup){
-				app.storeinventory.init();
-			}
-			app.account.initCartLogin();
+	init : function () {
+		// edit shopping cart line item
+		initializeCache();
+		initializeEvents();
+		if(app.enabledStorePickup){
+			storeinventory.init();
 		}
-	};
+		account.initCartLogin();
+	}
+};
 
-}(window.app = window.app || {}, jQuery));
+module.exports = cart;
