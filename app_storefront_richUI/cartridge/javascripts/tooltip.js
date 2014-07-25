94d3e9558941e2fc195cdb18d639dd5ca3534eb3
Tri Nguyen <tri@tridnguyen.com>
2014-07-25 15:50:06 -0400

    commonjs tooltip

--- app_storefront_richUI/cartridge/javascripts/tooltip.js
+++ app_storefront_richUI/cartridge/javascripts/tooltip.js
@@ -1,29 +1,21 @@
+'use strict';
+
 /**
-@class app.tooltip
-*/
-(function (app, $) {
-	var $cache = {};
-	app.tooltips = {
-		/**
-		 * @function
-		 * @description Initializes the tooltip-content and layout
-		 */
-		init : function () {
-
-			$('.tooltip').tooltip({
-				track: true,
-				showURL: false,
-			    bodyHandler: function() {
-					// add a data attribute of data-layout="some-class" to your tooltip-content container if you want a custom class
-					var tooltipClass = "";
-					if( tooltipClass = $(this).find('.tooltip-content').data("layout") ) {
-						tooltipClass = " class='" + tooltipClass + "' ";
-					}
-		        	return "<div " + tooltipClass + ">" + $(this).find('.tooltip-content').html() + "</div>";
-				},
-				showURL: false
-			});
+ * @function
+ * @description Initializes the tooltip-content and layout
+ */
+exports.init = function () {
+	$('.tooltip').tooltip({
+		track: true,
+		showURL: false,
+		showURL: false,
+		bodyHandler: function() {
+			// add a data attribute of data-layout="some-class" to your tooltip-content container if you want a custom class
+			var tooltipClass = "";
+			if (tooltipClass = $(this).find('.tooltip-content').data("layout")) {
+				tooltipClass = " class='" + tooltipClass + "' ";
+			}
+		return "<div " + tooltipClass + ">" + $(this).find('.tooltip-content').html() + "</div>";
 		}
-	};
-
-}(window.app = window.app || {}, jQuery));
+	});
+};
