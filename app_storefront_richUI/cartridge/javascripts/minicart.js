67fe729e22965a2dd5a53d69a07735783a92931a
Tri Nguyen <tri@tridnguyen.com>
2014-07-25 13:33:28 -0400

    commonjs minicart.js

--- app_storefront_richUI/cartridge/javascripts/minicart.js
+++ app_storefront_richUI/cartridge/javascripts/minicart.js
@@ -1,112 +1,88 @@
-/**
- * @class app.minicart
- */
-(function (app, $) {
-	// sub name space app.minicart.* provides functionality around the mini cart
+'use strict';
 
-	var $cache = {},
-		initialized = false;
+var util = require('./util'),
+	bonusProductsView = require('./bonus-products-view');
 
-	var timer = {
-		id : null,
-		clear : function () {
-			if(timer.id) {
-				window.clearTimeout(timer.id);
-				delete timer.id;
-			}
-		},
-		start : function (duration) {
-			timer.id = setTimeout(app.minicart.close, duration);
+var timer = {
+	id : null,
+	clear : function () {
+		if (this.id) {
+			window.clearTimeout(this.id);
+			delete this.id;
 		}
-	};
-	/******* app.minicart public object ********/
-	app.minicart = {
-		url : "", // during page loading, the Demandware URL is stored here
+	},
+	start : function (duration, callback) {
+		this.id = setTimeout(callback, duration);
+	}
+};
+/******* app.minicart public object ********/
+var minicart = {
+	$el: $('#mini-cart'),
 
-		/**
-		 * @function
-		 * @description Cache initializations and event binding to the mimcart
-		 */
-		init : function () {
-			$cache.minicart = $("#mini-cart");
-			$cache.mcTotal = $cache.minicart.find(".mini-cart-total");
-			$cache.mcContent = $cache.minicart.find(".mini-cart-content");
-			$cache.mcClose = $cache.minicart.find(".mini-cart-close");
-			$cache.mcProductList = $cache.minicart.find(".mini-cart-products");
-			$cache.mcProducts = $cache.mcProductList.children(".mini-cart-product");
+	$content: this.$el.find('.mini-cart-content'),
 
-			var collapsed = $cache.mcProductList.children().not(":first").addClass("collapsed");
+	init : function () {
+		$cache.mcTotal = $cache.minicart.find(".mini-cart-total");
+		$cache.mcContent = $cache.minicart.find(".mini-cart-content");
+		$cache.mcClose = $cache.minicart.find(".mini-cart-close");
+		$cache.mcProductList = $cache.minicart.find(".mini-cart-products");
+		$cache.mcProducts = $cache.mcProductList.children(".mini-cart-product");
 
 
-			// bind hover event to the cart total link at the top right corner
-			$cache.minicart.on("mouseenter", ".mini-cart-total", function () {
-				if($cache.mcContent.not(":visible")) {
-					app.minicart.slide();
-				}
+		var $productList = this.$el.find('.mini-cart-products');
+		$productList.children().not(':first').addClass('collapsed');
+		$productList.find('.mini-cart-product').append('<div class="mini-cart-toggler">&nbsp;</div>');
+
+		$productList.toggledList({toggleClass : "collapsed", triggerSelector:".mini-cart-toggler", eventName:"click"});
+
+		// events
+		this.$el.find('.mini-cart-total').on('mouseenter', function () {
+			if (this.$content.not(':visible') {
+				this.slide();
 			})
-			.on("mouseenter", ".mini-cart-content", function (e) {
-				timer.clear();
-			})
-			.on("mouseleave", ".mini-cart-content", function (e) {
-				timer.clear();
-				timer.start(30);
-			})
-			.on("click", ".mini-cart-close", app.minicart.close);
+		}.bind(this));
 
-			$cache.mcProducts.append('<div class="mini-cart-toggler">&nbsp;</div>');
-
-			$cache.mcProductList.toggledList({toggleClass : "collapsed", triggerSelector:".mini-cart-toggler", eventName:"click"});
-
-			initialized = true;
-		},
-		/**
-		 * @function
-		 * @description Shows the given content in the mini cart
-		 * @param {String} A HTML string with the content which will be shown
-		 */
-		show : function (html) {
-			$cache.minicart.html(html);
-			app.util.scrollBrowser(0);
-			app.minicart.init();
-			app.minicart.slide();
-			app.bonusProductsView.loadBonusOption();
-		},
-		/**
-		 * @function
-		 * @description Slides down and show the contents of the mini cart
-		 */
-		slide : function () {
-			if(!initialized) {
-				app.minicart.init();
-			}
-
-			if(app.minicart.suppressSlideDown && app.minicart.suppressSlideDown()) {
-				return;
-			}
-
+		this.$content.on('mouseenter', function () {
 			timer.clear();
-
-			// show the item
-			$cache.mcContent.slideDown('slow');
-
-			// after a time out automatically close it
-			timer.start(6000);
-		},
-		/**
-		 * @function
-		 * @description Closes the mini cart with given delay
-		 * @param {Number} delay The delay in milliseconds
-		 */
-		close : function (delay) {
+		}).on('mouseleave', function () {
 			timer.clear();
-			$cache.mcContent.slideUp();
-		},
-		/**
-		 * @function
-		 * @description Hook which can be replaced by individual pages/page types (e.g. cart)
-		 */
-			suppressSlideDown : function () {
-			return false;
-		}
-	};
-}(window.app = window.app || {}, jQuery));
+			timer.start(30, this.close);
+		}.bind(this));
+
+		this.$el.find('.mini-cart-close').on('click', this.close);
+	},
+	/**
+	 * @function
+	 * @description Shows the given content in the mini cart
+	 * @param {String} A HTML string with the content which will be shown
+	 */
+	show : function (html) {
+		this.$el.html(html);
+		util.scrollBrowser(0);
+		this.init();
+		this.slide();
+		bonusProductsView.loadBonusOption();
+	},
+	/**
+	 * @function
+	 * @description Slides down and show the contents of the mini cart
+	 */
+	slide : function () {
+		timer.clear();
+		// show the item
+		this.$content.slideDown('slow');
+		// after a time out automatically close it
+		timer.start(6000, this.close);
+	},
+	/**
+	 * @function
+	 * @description Closes the mini cart with given delay
+	 * @param {Number} delay The delay in milliseconds
+	 */
+	close : function (delay) {
+		timer.clear();
+		this.$content.slideUp(delay);
+	},
+};
+
+module.exports = minicart;
