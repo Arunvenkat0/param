eb23ebb39c32e0815220e920848426858b0788ec
Tri Nguyen <tri@tridnguyen.com>
2014-07-25 15:54:30 -0400

    commonjs wishlist

--- app_storefront_richUI/cartridge/javascripts/wishlist.js
+++ app_storefront_richUI/cartridge/javascripts/wishlist.js
@@ -1,39 +1,19 @@
-/**
- * @class app.wishlist
- */
-(function (app, $) {
-	var $cache = {};
-	/**
-	 * @private
-	 * @function
-	 * @description Binds the send to friend and address changed events to the wishlist page
-	 */
-	function initializeEvents() {
-		app.sendToFriend.initializeDialog("div.list-table-header", ".send-to-friend");
-		$cache.editAddress.on('change', function () {
-			window.location.href = app.util.appendParamToURL(app.urls.wishlistAddress, "AddressID", $(this).val());
+'use strict';
 
-		});
-		
-		//add js logic to remove the , from the qty feild to pass regex expression on client side
-		jQuery('.option-quantity-desired div input').focusout(function(){		
-			$(this).val($(this).val().replace(',',''));	
-		});
-	}
+var page = require('./page'),
+	product = require('./product'),
+	sendToFiend = require('./send-to-friend'),
+	util = require('./util');
 
-
-	/******* app.wishlist public object ********/
-	app.wishlist = {
-		/**
-		 * @function
-		 * @description Binds events to the wishlist page
-		 */
-		init : function () {
-			$cache.editAddress = $('#editAddress');
-			$cache.wishlistTable = $('.pt_wish-list .item-list');
-			app.product.initAddToCart();
-			initializeEvents();
-
-		}
-	};
-}(window.app = window.app || {}, jQuery));
+exports.init = function () {
+	product.initAddToCart();
+	sendToFriend.initializeDialog(".list-table-header", ".send-to-friend");
+	$('#editAddress').on('change', function () {
+		page.redirect(util.appendParamToURL(app.urls.wishlistAddress, "AddressID", $(this).val()));
+	});
+	
+	//add js logic to remove the , from the qty feild to pass regex expression on client side
+	$('.option-quantity-desired input').on('focusout', function () {
+		$(this).val($(this).val().replace(',',''));	
+	});
+};
