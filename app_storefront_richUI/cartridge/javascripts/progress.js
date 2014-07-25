6d3d2366e580cc4c60d8b424d89f6d34432ede59
Tri Nguyen <tri@tridnguyen.com>
2014-07-25 15:47:13 -0400

    commonjs progress

--- app_storefront_richUI/cartridge/javascripts/progress.js
+++ app_storefront_richUI/cartridge/javascripts/progress.js
@@ -1,31 +1,31 @@
+'use strict';
+
+var $loader;
+
 /**
- * @class app.progress
+ * @function
+ * @description Shows an AJAX-loader on top of a given container
+ * @param {Element} container The Element on top of which the AJAX-Loader will be shown
  */
-(function (app, $) {
-	var loader;
-	app.progress = {
-		/**
-		 * @function
-		 * @description Shows an AJAX-loader on top of a given container
-		 * @param {Element} container The Element on top of which the AJAX-Loader will be shown
-		 */
-		show: function (container) {
-			var target = (!container || $(container).length===0) ? $("body") : $(container);
-			loader = loader || $(".loader");
+var show = function (container) {
+	var target = (!container || $(container).length === 0) ? $("body") : $(container);
+	$loader = $loader || $(".loader");
 
-			if (loader.length===0) {
-				loader = $("<div/>").addClass("loader")
-									.append($("<div/>").addClass("loader-indicator"), $("<div/>").addClass("loader-bg"));
+	if ($loader.lengt === 0) {
+		$loader = $("<div/>").addClass("loader")
+			.append($("<div/>").addClass("loader-indicator"), $("<div/>").addClass("loader-bg"));
+	}
+	return $loader.appendTo(target).show();
+},
+/**
+ * @function
+ * @description Hides an AJAX-loader
+ */
+var hide = function () {
+	if ($loader) { 
+		$loader.hide(); 
+	}
+}
 
-			}
-			return loader.appendTo(target).show();
-		},
-		/**
-		 * @function
-		 * @description Hides an AJAX-loader
-		 */
-		hide: function () {
-			if (loader) { loader.hide(); }
-		}
-	};
-}(window.app = window.app || {}, jQuery));
+exports.show = show;
+exports.hide = hide;
