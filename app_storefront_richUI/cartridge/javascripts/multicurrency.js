0d0c413f7229eed589aa12094ecb84891f4f6b84
Tri Nguyen <tri@tridnguyen.com>
2014-07-25 13:35:57 -0400

    commonjs multicurrency.js

--- app_storefront_richUI/cartridge/javascripts/multicurrency.js
+++ app_storefront_richUI/cartridge/javascripts/multicurrency.js
@@ -1,39 +1,26 @@
-/**
- * @class app.mulitcurrency
- */
-(function (app, $) {
-	/**
-	 * @private
-	 * @function
-	 * @description Binds event to the place holder (.blur)
-	 */
-	function initializeEvents() {
-		//listen to the drop down, and make a ajax call to mulitcurrency pipeline
-		$('.currency-converter').on("change", function () {
- 			// request results from server
- 	 		app.ajax.getJson({
- 	 		 	url: app.util.appendParamsToUrl(app.urls.currencyConverter , {format:"ajax",currencyMnemonic:$('select.currency-converter').val()}),
- 	 		 	callback: function(data){
- 	 				location.reload();
- 	 		 	}// end ajax callback
- 	 		 });
-		});
-		
-		//hide the feature if user is in checkout
-		if(app.page.title=="Checkout"){
-			$('.mc-class').css('display','none');
-		}
-		
-	}
+'use strict';
 
-	/******* app.mulitcurrency public object ********/
-	app.mulitcurrency = {
-		/**
-		 * @function
-		 * @description 
-		 */
-		init : function () {
-			initializeEvents();
-		}
-	};
-}(window.app = window.app || {}, jQuery));
+var ajax = require('./ajax'),
+	page = require('./page'),
+	util = require('./util')
+
+exports.init = function () {
+	//listen to the drop down, and make a ajax call to mulitcurrency pipeline
+	$('.currency-converter').on('change', function () {
+			// request results from server
+	 		ajax.getJson({
+	 		 	url: util.appendParamsToUrl(app.urls.currencyConverter , {
+	 		 		format: 'ajax',
+	 		 		currencyMnemonic: $('.currency-converter').val()
+	 		 	}),
+	 		 	callback: function(){
+	 				location.reload();
+	 		 	}
+	 		 });
+	});
+	
+	//hide the feature if user is in checkout
+	if (page.title=="Checkout"){
+		$('.mc-class').css('display','none');
+	}
+};
