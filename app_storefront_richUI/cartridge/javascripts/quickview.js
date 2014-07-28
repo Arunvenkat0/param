85bbd0ebb110e91d59445fb89aa303b3a57dd9de
Tri Nguyen <tri@tridnguyen.com>
2014-07-28 15:27:56 -0400

    commonjs quickview

--- app_storefront_richUI/cartridge/javascripts/quickview.js
+++ app_storefront_richUI/cartridge/javascripts/quickview.js
@@ -1,178 +1,150 @@
-/**
- * @class app.quickview
- */
-(function (app, $) {
-	var $cache = {};
-	/**
-	 * @function
-	 * @description Binds a 'click'-event to the quick view button
-	 */
-	function bindQvButton() {
-		$cache.qvButton.one("click", function (e) {
-			e.preventDefault();
-			app.quickView.show({
-				url : $(this).attr("href"),
-				source : "quickview"
-			});
-		});
-	}
+'use strict';
 
-	/******* app.quickView public object ********/
-	app.quickView = {
-		/**
-		 * @function
-		 * @description
-		 */
-		initializeButton : function (container, target) {
-			// quick view button
-			$(container).on("mouseenter", target, function (e) {
-				if(!$cache.qvButton) {
-					$cache.qvButton = $("<a id='quickviewbutton'/>");
-				}
-				bindQvButton();
+var dialog = require('./dialog'),
+	product = require('./product'),
+	progress = require('./progress');
 
-				var link = $(this).children("a:first");
-				$cache.qvButton.attr({
-					"href" : link.attr("href"),
-					"title" : link.attr("title")
-				}).appendTo($(this));
-			});
-		},
-		init : function () {
-			if(app.quickView.exists()) {
-				return $cache.quickView;
+var quickView = {
+	initializeButton : function (container, target) {
+		var that = this;
+		// quick view button
+		$(container).on("mouseenter", target, function (e) {
+			var $qvButton = $('#quickviewbutton');
+			if ($qvButton.length === 0) {
+				$qvButton = $('<a id="quickviewbutton"/>');
 			}
-
-			$cache.quickView = $("<div/>").attr("id", "#QuickViewDialog").appendTo(document.body);
-			return $cache.quickView;
-		},
-		
-		initializeQuickViewNav : function(qvUrl) {
-			
-			//from the url of the product in the quickview
-			qvUrlTail = qvUrl.substring(qvUrl.indexOf('?'));
-			qvUrlPidParam = qvUrlTail.substring(0,qvUrlTail.indexOf('&'));
-			qvUrl = qvUrl.substring(0, qvUrl.indexOf('?'));
-			
-			if(qvUrlPidParam.indexOf('pid') > 0){
-				//if storefront urls are turned off
-				//append the pid to the url
-				qvUrl = qvUrl+qvUrlPidParam;
-			}
-			
-			this.searchesultsContainer = $('#search-result-items').parent();
-			this.productLinks = this.searchesultsContainer.find('.thumb-link');
-
-			this.btnNext = $('.quickview-next');
-			this.btnPrev = $('.quickview-prev');
-
-			this.btnNext.click(this.navigateQuickview.bind(this));
-			this.btnPrev.click(this.navigateQuickview.bind(this));
-
-			var productLinksUrl = "";
-			for ( var i = 0; i < this.productLinks.length; i++) {
-
-				productLinksUrlTail = this.productLinks[i].href.substring(this.productLinks[i].href.indexOf('?'));
-				productLinksUrlPidParam = productLinksUrlTail.substring(0,qvUrlTail.indexOf('&'));
-				if(productLinksUrlPidParam.indexOf('pid') > 0){
-					//append the pid to the url
-					//if storefront urls are turned off
-					productLinksUrl = this.productLinks[i].href.substring(0, this.productLinks[i].href.indexOf('?'));
-					productLinksUrl = productLinksUrl+productLinksUrlPidParam;
-				
-				}else{
-					productLinksUrl = this.productLinks[i].href.substring(0, this.productLinks[i].href.indexOf('?'));
-				}
-			
-				if(productLinksUrl == ""){
-					productLinksUrl = this.productLinks[i].href;
-				}
-				
-				if (qvUrl == productLinksUrl) {
-					this.productLinkIndex = i;
-				}
-			}
-
-			if (this.productLinkIndex == this.productLinks.length - 1) {
-				this.btnNext.hide();
-			}
-
-			if (this.productLinkIndex == 0) {
-				this.btnPrev.hide();
-			}
-
-			//hide the buttons on the compare page
-			if($('.compareremovecell').length > 0){
-				this.btnNext.hide();
-				this.btnPrev.hide();
-			}
-			
-		},
-
-		navigateQuickview : function(event) {
-			var button = $(event.currentTarget);
-
-			if (button.hasClass('quickview-next')) {
-				this.productLinkIndex++;
-			} else {
-				this.productLinkIndex--;
-			}
-
-			app.quickView.show({
-				url : this.productLinks[this.productLinkIndex].href,
-				source : 'quickview'
-			});
-
-			event.preventDefault();
-		},
-		
-		// show quick view dialog and send request to the server to get the product
-		// options.source - source of the dialog i.e. search/cart
-		// options.url - product url
-		/**
-		 * @function
-		 * @description
-		 */
-		show : function (options) {
-			options.target = app.quickView.init();
-			options.callback = function () {
-				app.product.init();
-				app.dialog.create({
-					target : $cache.quickView,
-					options : {
-						height : 'auto',
-						width : 920,
-						dialogClass : 'quickview',
-						title : 'Product Quickview',
-						resizable : false,
-						position : 'center',
-						open : function () {
-							app.progress.hide();
-						}
-					}
+			$qvButton.on("click", function (e) {
+				e.preventDefault();
+				that.show({
+					url : $(this).attr("href"),
+					source : "quickview"
 				});
-				$cache.quickView.dialog('open');
-				
-				app.quickView.initializeQuickViewNav(this.url);
-			};
-			app.product.get(options);
+			});
 
-			return $cache.quickView;
-		},
-		// close the quick view dialog
-		close : function () {
-			if($cache.quickView) {
-				$cache.quickView.dialog('close').empty();
-				return $cache.quickView;
+			var $link = $(this).children("a:first");
+			$qvButton.attr({
+				'href': $link.attr('href'),
+				'title': $link.attr('title')
+			}).appendTo($(this));
+		});
+	},
+	init : function () {
+		if (!this.exists()) {
+			this.$container = $('<div/>').attr('id', '#QuickViewDialog').appendTo(document.body);
+		}
+	},
+	
+	initializeQuickViewNav : function(qvUrl) {
+		// from the url of the product in the quickview
+		var qvUrlTail = qvUrl.substring(qvUrl.indexOf('?')),
+			qvUrlPidParam = qvUrlTail.substring(0, qvUrlTail.indexOf('&'));
+		qvUrl = qvUrl.substring(0, qvUrl.indexOf('?'));
+		
+		if (qvUrlPidParam.indexOf('pid') > 0){
+			// if storefront urls are turned off
+			// append the pid to the url
+			qvUrl = qvUrl + qvUrlPidParam;
+		}
+		
+		this.searchesultsContainer = $('#search-result-items').parent();
+		this.productLinks = this.searchesultsContainer.find('.thumb-link');
+
+		this.btnNext = $('.quickview-next');
+		this.btnPrev = $('.quickview-prev');
+
+		this.btnNext.click(this.navigateQuickview.bind(this));
+		this.btnPrev.click(this.navigateQuickview.bind(this));
+
+		var productLinksUrl = '';
+		for (var i = 0; i < this.productLinks.length; i++) {
+			productLinksUrlTail = this.productLinks[i].href.substring(this.productLinks[i].href.indexOf('?'));
+			productLinksUrlPidParam = productLinksUrlTail.substring(0, qvUrlTail.indexOf('&'));
+			if (productLinksUrlPidParam.indexOf('pid') > 0){
+				//append the pid to the url
+				//if storefront urls are turned off
+				productLinksUrl = this.productLinks[i].href.substring(0, this.productLinks[i].href.indexOf('?'));
+				productLinksUrl = productLinksUrl + productLinksUrlPidParam;
+			} else {
+				productLinksUrl = this.productLinks[i].href.substring(0, this.productLinks[i].href.indexOf('?'));
 			}
-		},
-		exists : function () {
-			return $cache.quickView && ($cache.quickView.length > 0);
-		},
-		isActive : function () {
-			return $cache.quickView && ($cache.quickView.length > 0) && ($cache.quickView.children.length > 0);
-		},
-		container : $cache.quickView
-	};
+		
+			if (productLinksUrl == ''){
+				productLinksUrl = this.productLinks[i].href;
+			}
+			if (qvUrl == productLinksUrl) {
+				this.productLinkIndex = i;
+			}
+		}
 
-}(window.app = window.app || {}, jQuery));
+		if (this.productLinkIndex == this.productLinks.length - 1) {
+			this.btnNext.hide();
+		}
+
+		if (this.productLinkIndex == 0) {
+			this.btnPrev.hide();
+		}
+
+		//hide the buttons on the compare page
+		if ($('.compareremovecell').length > 0){
+			this.btnNext.hide();
+			this.btnPrev.hide();
+		}
+	},
+	navigateQuickview : function (e) {
+		e.preventDefault();
+		var button = $(e.currentTarget);
+
+		if (button.hasClass('quickview-next')) {
+			this.productLinkIndex++;
+		} else {
+			this.productLinkIndex--;
+		}
+
+		this.show({
+			url : this.productLinks[this.productLinkIndex].href,
+			source : 'quickview'
+		});
+	},
+	
+	// show quick view dialog and send request to the server to get the product
+	// options.source - source of the dialog i.e. search/cart
+	// options.url - product url
+	show : function (options) {
+		var that = this;
+		options.target = this.init();
+		options.callback = function () {
+			product.init();
+			dialog.create({
+				target : that.$container,
+				options : {
+					height : 'auto',
+					width : 920,
+					dialogClass : 'quickview',
+					title : 'Product Quickview',
+					resizable : false,
+					position : 'center',
+					open : function () {
+						progress.hide();
+					}
+				}
+			});
+			that.$container.dialog('open');
+			that.initializeQuickViewNav(this.url);
+		};
+		product.get(options);
+	},
+	// close the quick view dialog
+	close : function () {
+		if(this.exists()) {
+			this.$container.dialog('close').empty();
+		}
+	},
+	exists : function () {
+		return this.$container && (this.$container.length > 0);
+	},
+	isActive : function () {
+		return this.exists() && (this.$container.children.length > 0);
+	}
+};
+
+module.exports = quickview;
