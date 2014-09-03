360e602f22beafbdb869634ad2c1718451989dba
Tri Nguyen <tri@tridnguyen.com>
2014-09-03 15:31:39 -0400

    Use promise to sequence clear actions.
    Use jQuery ajax directly to avoid ajax module's current request check.

--- app_storefront_richUI/cartridge/js/product-compare.js
+++ app_storefront_richUI/cartridge/js/product-compare.js
@@ -2,12 +2,11 @@
 
 var ajax = require('./ajax'),
 	page = require('./page'),
-	util = require('./util');
+	util = require('./util'),
+	Promise = require('promise');
 
-var _currentCategory = "",
-	_isClearing = false,
-	MAX_ACTIVE = 6,
-	CI_PREFIX = "ci-";
+var _currentCategory = '',
+	MAX_ACTIVE = 6;
 
 /**
  * @private
@@ -16,24 +15,23 @@ var _currentCategory = "",
  */
 function refreshContainer() {
 	var $compareContainer = $('#compare-items');
-	if (_isClearing) { return; }
+	var $compareItems = $compareContainer.find('.compare-item');
+	var numActive = $compareItems.filter('.active').length;
 
-	var ac = $compareContainer.find(".active").length;
-
-	if (ac < 2) {
-		$("#compare-items-button").attr("disabled", "disabled");
+	if (numActive < 2) {
+		$('#compare-items-button').attr('disabled', 'disabled');
 	} else {
-		$("#compare-items-button").removeAttr("disabled");
+		$('#compare-items-button').removeAttr('disabled');
 	}
 
 	// update list with sequential classes for ui targeting
-	var $compareItems = $compareContainer.find('.compare-item');
-	for (var i = 0; i < $compareItems.length; i++ ){
+
+	for (var i = 0; i < $compareItems.length; i++) {
 		$compareItems.removeClass('compare-item-' + i);
-		$($compareItems[i]).addClass('compare-item-' + i);
+		$compareItems.eq(i).addClass('compare-item-' + i);
 	}
 
-	$compareContainer.toggle(ac > 0);
+	$compareContainer.toggle(numActive > 0);
 
 }
 /**
@@ -43,37 +41,40 @@ function refreshContainer() {
  */
 function addToList(data) {
 	// get the first compare-item not currently active
-	var $item = $('#compare-items').find(".compare-item").not(".active").first(),
-		$tile = $('#' + data.uuid);
+	var $item = $('#compare-items').find('.compare-item').not('.active').first(),
+		$productTile = $('#' + data.uuid);
 
 	if ($item.length === 0) {
-		if ($tile.length > 0) {
-			$tile.find(".compare-check")[0].checked = false;
+		if ($productTile.length > 0) {
+			$productTile.find('.compare-check')[0].checked = false;
 		}
 		window.alert(Resources.COMPARE_ADD_FAIL)
 		return;
 	} // safety only
 
 	// if already added somehow, return
-	if ($('#' + CI_PREFIX + data.uuid).length > 0) {
+	if ($('[data-uuid="' + data.uuid + '"]').length > 0) {
 		return;
 	}
 	// set as active item
-	$item.addClass("active")
-		.attr("id", CI_PREFIX+data.uuid)
-		.data("itemid", data.itemid);
+	$item.addClass('active')
+		.attr('data-uuid', data.uuid)
+		.data('itemid', data.itemid);
 
 	// replace the item image
-	$item.children(".compareproduct").first()
-		.attr({src : $(data.img).attr("src"), alt : $(data.img).attr("alt")});
+	$item.children('.compareproduct').first()
+		.attr({
+			src: $(data.img).attr('src'),
+			alt: $(data.img).attr('alt')
+		});
 
 	// refresh container state
 	refreshContainer();
 
-	if ($tile.length === 0) { return; }
+	if ($productTile.length === 0) { return; }
 
 	// ensure that the associated checkbox is checked
-	$tile.find(".compare-check")[0].checked = true;
+	$productTile.find('.compare-check')[0].checked = true;
 }
 /**
  * @private
@@ -81,17 +82,21 @@ function addToList(data) {
  * description Removes an item from the compare container and refreshes it
  */
 function removeFromList(uuid) {
-	var $item = $('#' + CI_PREFIX + uuid);
+	var $item = $('[data-uuid="' + uuid + '"]');
 	if ($item.length === 0) { return; }
 
 	// replace the item image
 	$item.children('.compareproduct').first()
-		.attr({src : Urls.compareEmptyImage, alt : Resources.EMPTY_IMG_ALT});
+		.attr({
+			src: Urls.compareEmptyImage,
+			alt: Resources.EMPTY_IMG_ALT
+		});
 
 	// remove class, data and id from item
 	$item.removeClass('active')
-		.removeAttr('id')
+		.removeAttr('data-uuid')
 		.removeAttr('data-itemid')
+		.data('uuid', '')
 		.data('itemid', '');
 
 	// use clone to prevent image flash when removing item from list
@@ -101,103 +106,112 @@ function removeFromList(uuid) {
 	refreshContainer();
 
 	// ensure that the associated checkbox is not checked
-	var $tile = $('#' + uuid);
-	if ($tile.length === 0 ) { return; }
-	$tile.find('.compare-check')[0].checked = false;
+	var $productTile = $('#' + uuid);
+	if ($productTile.length === 0 ) {return;}
+	$productTile.find('.compare-check')[0].checked = false;
 }
 
-/**
- * @function
- * @description Removes product from the compare table
- */
-function removeProduct (args) {
-	if (!args.itemid) { return; }
-	var cb = args.cb ? $(args.cb) : null;
-	var ajaxCall = args.ajaxCall ? $(args.ajaxCall) : true;
-	if (ajaxCall) {
-		ajax.getJson({
-			url : Urls.compareRemove,
-			data : { 'pid' : args.itemid, 'category' : _currentCategory },
-			callback : function (response) {
-				if (!response || !response.success) {
-					// response failed. uncheck the checkbox return
-					if (cb && cb.length > 0) { cb[0].checked = true; }
-					window.alert(Resources.COMPARE_REMOVE_FAIL);
-					return;
-				}
-
-				// item successfully removed session, now remove from to list...
-				removeFromList(args.uuid);
+function addProductAjax(args) {
+	var promise = new Promise(function (resolve, reject) {
+		$.ajax({
+			url: Urls.compareAdd,
+			data: {
+				pid: args.itemid,
+				category: _currentCategory
+			},
+			dataType: 'json',
+		}).done(function (response) {
+			if (!response || !response.success) {
+				reject(new Error(Resources.COMPARE_ADD_FAIL));
+			} else {
+				resolve(response);
 			}
-		});
-	} else {
-		ajax.getJson({
-			url : Urls.compareRemove,
-			// TODO this is ATROCIOUS
-			async: false,
-			data : { 'pid' : args.itemid, 'category' : _currentCategory },
-			callback : function (response) {
-				if (!response || !response.success) {
-					// response failed. uncheck the checkbox return
-					if (cb && cb.length > 0) { cb[0].checked = true; }
-					window.alert(Resources.COMPARE_REMOVE_FAIL);
-					return;
-				}
-
-				// item successfully removed session, now remove from to list...
-				removeFromList(args.uuid);
-			}
-		});
-	}
+		}).fail(function (jqxhr, status, err) {
+			reject(new Error(err));
+		})
+	});
+	return promise;
 }
 
 /**
  * @function
  * @description Adds product to the compare table
  */
-function addProduct (args) {
-	var $items = $('#compare-items').find(".compare-item");
-	var cb = $(args.cb);
-	var ac = $items.filter(".active").length;
-	if (ac===MAX_ACTIVE) {
-		if(!window.confirm(Resources.COMPARE_CONFIRMATION)) {
-			cb[0].checked = false;
+function addProduct(args) {
+	var promise;
+	var $items = $('#compare-items').find('.compare-item');
+	var $cb = $(args.cb);
+	var numActive = $items.filter('.active').length;
+	if (numActive === MAX_ACTIVE) {
+		if (!window.confirm(Resources.COMPARE_CONFIRMATION)) {
+			$cb[0].checked = false;
 			return;
 		}
 
 		// remove product using id
-		var item = $items.first();
-
-		// safety check only. should never occur.
-		if (item[0].id.indexOf(CI_PREFIX)!==0) {
-			cb[0].checked = false;
-			window.alert(Resources.COMPARE_ADD_FAIL);
-			return;
-		}
-		var uuid = item[0].id.substr(CI_PREFIX.length);
-		removeProduct({
-			itemid: item.data("itemid"),
+		var $item = $items.first();
+		var uuid = $item.data('uuid');
+		promise = removeProduct({
+			itemid: item.data('itemid'),
 			uuid: uuid,
-			cb: $("#"+uuid).find(".compare-check"),
-			ajaxCall: false
+			cb: $('#' + uuid).find('.compare-check')
 		});
+	} else {
+		promise = Promise.resolve(0);
 	}
-
-	ajax.getJson({
-		url: Urls.compareAdd,
-		data: { 'pid' : args.itemid, 'category' : _currentCategory },
-		callback: function (response) {
-			if (!response || !response.success) {
-				// response failed. uncheck the checkbox return
-				cb[0].checked = false;
-				window.alert(Resources.COMPARE_ADD_FAIL);
-				return;
-			}
-
-			// item successfully stored in session, now add to list...
+	return promise.then(function () {
+		refreshContainer();
+		return addProductAjax(args).then(function () {
 			addToList(args);
-		}
+		});
+	}).then(null, function (err) {
+		if ($cb && $cb.length > 0) {$cb[0].checked = false;}
+		window.alert(err.message);
 	});
+}
+
+/**
+ * @function
+ * @description Removes product from the compare table
+ */
+function removeProduct(args) {
+	if (!args.itemid) {return;}
+	var $cb = args.cb ? $(args.cb) : null;
+	var promise = new Promise(function (resolve, reject) {
+		$.ajax({
+			url: Urls.compareRemove,
+			data: {
+				pid: args.itemid,
+				category: _currentCategory
+			},
+			dataType: 'json'
+		}).done(function (response) {
+			if (!response || !response.success) {
+				reject(new Error(Resources.COMPARE_REMOVE_FAIL));
+			} else {
+				resolve(response);
+			}
+		}).fail(function (jqxhr, status, err) {
+			reject(new Error(err));
+		});
+	});
+	return promise.then(function () {
+		removeFromList(args.uuid);
+	}, function (err) {
+		if ($cb && $cb.length > 0) {$cb[0].checked = true;}
+		window.alert(err.message);
+	});
+}
+
+function removeItem($item) {
+	var uuid = $item.data('uuid'),
+		$productTile = $('#' + uuid);
+	removeProduct({
+		itemid: $item.data('itemid'),
+		uuid: uuid,
+		cb: ($productTile.length === 0) ? null : $productTile.find('.compare-check')
+	});
+	refreshContainer();
 }
 
 /**
@@ -207,18 +221,17 @@ function addProduct (args) {
  */
 function initializeDom() {
 	var $compareContainer = $('#compare-items');
-	_currentCategory = $compareContainer.data("category") || "";
-	var $active = $compareContainer.find(".compare-item").filter(".active");
+	_currentCategory = $compareContainer.data('category') || '';
+	var $active = $compareContainer.find('.compare-item').filter('.active');
 	$active.each(function () {
-		var uuid = this.id.substr(CI_PREFIX.length),
-			$tile = $('#' + uuid);
-		if ($tile.length === 0 ) { return; }
-
-		$tile.find(".compare-check")[0].checked = true;
+		var $proudctTile = $('#' +  $(this).data('uuid'));
+		if ($productTile.length === 0) {return;}
+		$productTile.find('.compare-check')[0].checked = true;
 	});
 	// set container state
 	refreshContainer();
 }
+
 /**
  * @private
  * @function
@@ -227,31 +240,20 @@ function initializeDom() {
 function initializeEvents() {
 	// add event to buttons to remove products
 	$('.compare-item-remove').on('click', function (e) {
-		var $item = $(this).closest('.compare-item'),
-			uuid = $item[0].id.substr(CI_PREFIX.length),
-			$tile = $('#' + uuid);
-
-		removeProduct( {
-			itemid : $item.data('itemid'),
-			uuid : uuid,
-			cb :  ($tile.length === 0) ? null : $tile.find('.compare-check')
-		});
-		refreshContainer();
+		removeItem($(this).closest('.compare-item'));
 	});
 
 	// Button to go to compare page
 	$('#compare-items-button').on('click', function () {
-		page.redirect(util.appendParamToURL(Urls.compareShow, "category", _currentCategory));
+		page.redirect(util.appendParamToURL(Urls.compareShow, 'category', _currentCategory));
 	});
 
 	// Button to clear all compared items
-	$('#clear-compared-items').on("click", function () {
-		// TODO: this _isClearing business seems like a hack to me --Tri
-		_isClearing = true;
-		$('#compare-items').hide()
-			.find(".active .compare-item-remove")
-			.trigger("click", [false]);
-		_isClearing = false;
+	// rely on refreshContainer to take care of hiding the container
+	$('#clear-compared-items').on('click', function () {
+		$('#compare-items .active').each(function () {
+			removeItem($(this));
+		});
 	});
 }
 
