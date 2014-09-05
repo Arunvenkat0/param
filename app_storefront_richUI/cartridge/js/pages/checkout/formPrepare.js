837b410c6a05c80b2a17fb470ebee616a2c6dae1
Tri Nguyen <tri@tridnguyen.com>
2014-09-05 14:29:59 -0400

    prevent duplicate event handlers

--- app_storefront_richUI/cartridge/js/pages/checkout/formPrepare.js
+++ app_storefront_richUI/cartridge/js/pages/checkout/formPrepare.js
@@ -12,7 +12,7 @@ exports.init = function (continueSelector, formSelector) {
 	var validator = $form.validate();
 	var $requiredInputs = $('.required', $form).find(':input');
 	// check for required input
-	var hasEmptyRequired = function () {
+	function hasEmptyRequired() {
 		// filter out only the visible fields - this allows the checking to work on
 		// billing page where some payment methods inputs are hidden
 		var requiredValues = $requiredInputs.filter(':visible').map(function () {
@@ -21,6 +21,7 @@ exports.init = function (continueSelector, formSelector) {
 		return $.inArray('', requiredValues) !== -1;
 	};
 
+	// validate form on init
 	if (!hasEmptyRequired()) {
 		// only validate form when all required fields are filled to avoid
 		// throwing errors on empty form
@@ -31,7 +32,7 @@ exports.init = function (continueSelector, formSelector) {
 		$continue.attr('disabled', 'disabled');
 	}
 
-	$requiredInputs.on('change', function () {
+	function validateInputs() {
 		if ($(this).val() === '') {
 			$continue.attr('disabled', 'disabled');
 		} else {
@@ -43,5 +44,7 @@ exports.init = function (continueSelector, formSelector) {
 				$continue.attr('disabled', 'disabled');
 			}
 		}
-	});
+	}
+
+	$requiredInputs.off('change', validateInputs).on('change', validateInputs);
 }
