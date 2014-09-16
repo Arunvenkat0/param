674682bb804eaf71c75de145096f6c8f1092d0e7
Larry Gelberg <lgelberg@demandware.com>
2014-09-16 11:53:13 -0400

    hid the product leak and made the changes very minimal

--- app_storefront_core/cartridge/scripts/cart/calculate.js
+++ app_storefront_core/cartridge/scripts/cart/calculate.js
@@ -8,6 +8,81 @@
  */
 
 /**
+ * @function calculate
+ *
+ * calculate is the arching logic for computing the value of a basket.  It makes
+ * calls into cart/calculate.js and enables both SG and OCAPI applications to share
+ * the same cart calculation logic.
+ *
+ * @param {object} basket The basket to be calculated
+ */
+exports.calculate = function(basket)
+{
+    // ===================================================
+	// =====   CALCULATE PRODUCT LINE ITEM PRICES    =====
+    // ===================================================
+
+	cartCalculator.calculateProductPrices(basket);
+
+    // ===================================================
+	// =====    CALCULATE GIFT CERTIFICATE PRICES    =====
+    // ===================================================
+
+	cartCalculator.calculateGiftCertificatePrices(basket);
+
+    // ===================================================
+	// =====   Note: Promotions must be applied      =====
+	// =====   after the tax calculation for         =====
+	// =====   storefronts based on GROSS prices     =====
+    // ===================================================
+
+    // ===================================================
+	// =====   APPLY PROMOTION DISCOUNTS			 =====
+	// =====   Apply product and order promotions.   =====
+	// =====   Must be done before shipping 		 =====
+	// =====   calculation. 					     =====
+    // ===================================================
+
+	PromotionMgr.applyDiscounts(basket);
+
+    // ===================================================
+	// =====        CALCULATE SHIPPING COSTS         =====
+    // ===================================================
+
+	// apply product specific shipping costs
+	// and calculate total shipping costs
+	ShippingMgr.applyShippingCost(basket);
+
+    // ===================================================
+	// =====   APPLY PROMOTION DISCOUNTS			 =====
+	// =====   Apply product and order and 			 =====
+	// =====   shipping promotions.                  =====
+    // ===================================================
+
+	PromotionMgr.applyDiscounts(basket);
+
+	// since we might have bonus product line items, we need to
+	// reset product prices
+	cartCalculator.calculateProductPrices(basket);
+
+    // ===================================================
+	// =====         CALCULATE TAX                   =====
+    // ===================================================
+
+	cartCalculator.calculateTax(basket);
+
+    // ===================================================
+	// =====         CALCULATE BASKET TOTALS         =====
+    // ===================================================
+
+	basket.updateTotals();
+
+    // ===================================================
+	// =====            DONE                         =====
+    // ===================================================
+}
+
+/**
  * @function calculateProductPrices
  *
  * Calculates product prices based on line item quantities. Set calculates prices
@@ -15,7 +90,7 @@
  *
  * @param {object} basket The basket containing the elements to be computed
  */
-exports.calculateProductPrices = function(basket)
+function calculateProductPrices (basket)
 {
 	// get total quantities for all products contained in the basket
 	var productQuantities : HashMap = basket.getProductQuantities();
@@ -105,7 +180,7 @@ exports.calculateProductPrices = function(basket)
  *
  * @param {object} basket The basket containing the gift certificates
  */
-exports.calculateGiftCertificatePrices = function(basket)
+function calculateGiftCertificatePrices (basket)
 {
 	var giftCertificates : Iterator = basket.getGiftCertificateLineItems().iterator();
 	while(giftCertificates.hasNext())
@@ -134,7 +209,7 @@ exports.calculateGiftCertificatePrices = function(basket)
  *
  * @param {object} basket The basket containing the elements for which taxes need to be calculated
  */
-exports.calculateTax = function(basket)
+function calculateTax (basket)
 {
 	var shipments : Iterator = basket.getShipments().iterator();
 	while(shipments.hasNext())
