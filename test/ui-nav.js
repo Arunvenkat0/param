884db10ab9392b54d16728c2d499b424b1ef4db9
Tri Nguyen <tri@tridnguyen.com>
2014-09-26 13:52:05 -0400

    promise-ify

--- test/ui-nav.js
+++ test/ui-nav.js
@@ -1,14 +1,18 @@
 var assert = require('chai').assert;
 var client = require('./browser/client');
 var config = require('./browser/config');
+var Promise = require('promise');
+
+var getAttribute = Promise.denodeify(client.getAttribute.bind(client));
 
 describe('navigation menu', function () {
 	before(function (done) {
 		client.init().url(config.url, done);
 	});
-	it('check navigation menu', function (done) {
-		client.getAttribute('#navigation .menu-category .level-1', 'innerHTML', function (err, res) {
-			assert.deepEqual(res, [
+	it('check navigation menu', function () {
+		return getAttribute('#navigation .menu-category .level-1', 'innerHTML').then(function (array) {
+			console.log(array);
+			assert.deepEqual(array, [
 				'New Arrivals',
 				'Womens',
 				'Mens',
@@ -16,8 +20,19 @@ describe('navigation menu', function () {
 				'Gift Certificates',
 				'Top Seller'
 			]);
-			done();
 		});
+
+		// , function (err, res) {
+		// 	assert.deepEqual(res, [
+		// 		'New Arrivals',
+		// 		'Womens',
+		// 		'Mens',
+		// 		'Electronics',
+		// 		'Gift Certificates',
+		// 		'Top Seller'
+		// 	]);
+		// 	done();
+		// });
 	});
 	after(function (done) {
 		client.end();
