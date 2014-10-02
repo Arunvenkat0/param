a380afc500b8619d5bf1f484c2f34e411cd5a41b
Tri Nguyen <tri@tridnguyen.com>
2014-10-02 15:55:35 -0400

    fix store pickup bug

--- app_storefront_richUI/cartridge/js/storeinventory.js
+++ app_storefront_richUI/cartridge/js/storeinventory.js
@@ -8,26 +8,27 @@ var pid,
 	currentTemplate = $('#wrapper.pt_cart').length ? 'cart' : 'pdp';
 
 var storeinventory = {
-	init : function () {
-		var that = this;
+	init: function () {
+		var self = this;
+		this.$preferredStorePanel = $('<div id="preferred-store-panel">');
 		// check for items that trigger dialog
 		$('#cart-table .set-preferred-store').on('click', function (e) {
 			e.preventDefault();
-			that.loadPreferredStorePanel($(this).parent().attr('id'));
+			self.loadPreferredStorePanel($(this).parent().attr('id'));
 		});
-		
+
 		//disable the radio button for home deliveries if the store inventory is out of stock
 		$('#cart-table .item-delivery-options .home-delivery .not-available').each(function(){
 			$(this).parents('.home-delivery').children('input').attr('disabled','disabled');
 		});
 
 		$('body').on('click', '#pdpMain .set-preferred-store', function (e) {
-			that.loadPreferredStorePanel($(this).parent().attr('id'));
+			self.loadPreferredStorePanel($(this).parent().attr('id'));
 			return false;
 		});
 
 		$('.item-delivery-options input.radio-url').on('click', function () {
-			that.setLineItemStore($(this));
+			self.setLineItemStore($(this));
 		});
 
 		if ($(".checkout-shipping").length > 0) {
@@ -37,10 +38,10 @@ var storeinventory = {
 		//disable the cart button if there is pli set to instore and the status is 'Not Available' and it is marked as an instore pli
 		$('.item-delivery-options').each(function () {
 			var $instore = $(this).children('.instore-delivery');
-			if (($instore.children("input").attr('disabled') == 'disabled')
+			if (($instore.children('input').attr('disabled') === 'disabled')
 				&& ($instore.children('.selected-store-availability').children('.store-error').length > 0)
-				&& ($instore.children("input").attr('checked') == 'checked')) {
-				$('.cart-action-checkout button').attr("disabled", "disabled");
+				&& ($instore.children('input').attr('checked') === 'checked')) {
+				$('.cart-action-checkout button').attr('disabled', 'disabled');
 			}
 		});
 	},
@@ -61,10 +62,10 @@ var storeinventory = {
 		var countplis = 0;
 		$('.item-delivery-options').each(function(){
 			var $instore = $(this).children('.instore-delivery');
-			if (($instore.children("input").attr('disabled')=='disabled')
+			if (($instore.children('input').attr('disabled') === 'disabled')
 				&& ($instore.children('.selected-store-availability').children('.store-error').length > 0)
-				&& ($instore.children("input").attr('checked')=='checked')){
-					$('.cart-action-checkout button').attr("disabled", "disabled");
+				&& ($instore.children('input').attr('checked') === 'checked')) {
+					$('.cart-action-checkout button').attr('disabled', 'disabled');
 				} else {
 					countplis++;
 				}
@@ -74,14 +75,17 @@ var storeinventory = {
 			}
 	},
 	buildStoreList: function (pid) {
-		var that = this,
-			$storeList = $('.store-list');
+		var self = this;
+		this.$storeList = $('<div class="store-list">');
 		// request results from server
 		ajax.getJson({
-			url: util.appendParamsToUrl(Urls.storesInventory , {pid:pid, zipCode: User.zip}),
+			url: util.appendParamsToUrl(Urls.storesInventory, {
+				pid: pid,
+				zipCode: User.zip
+			}),
 			callback: function (data) {
 				// clear any previous results, then build new
-				$storeList.empty();
+				self.$storeList.empty();
 				var listings = $("<ul class='store-list'/>");
 				if (data && data.length > 0) {
 					for (var i = 0; i < 10 && i < data.length; i++) {
@@ -105,8 +109,8 @@ var storeinventory = {
 					}
 				// no records
 				} else {
-					if (User.zip){
-						$storeList.append("<div class='no-results'>No Results</div>");
+					if (User.zip) {
+						self.$storeList.append("<div class='no-results'>No Results</div>");
 					}
 				}
 
@@ -116,8 +120,8 @@ var storeinventory = {
 					listingsNav = $('<div id="listings-nav"/>'),
 					selectedButtonText;
 				for (var i = 0, link = 1; i <= numListings; i++) {
-					if (numListings >  i) { 
-						listingsNav.append('<a data-index="'+ i +'">'+link+'</a>'); 
+					if (numListings >  i) {
+						listingsNav.append('<a data-index="'+ i +'">'+link+'</a>');
 					}
 					link++;
 					i = i + 2;
@@ -129,7 +133,7 @@ var storeinventory = {
 						'left': storeTileWidth * $(this).data('index') * - 1
 					}, 1000);
 				}).first().addClass('active');
-				$storeList.after(listingsNav);
+				self.$storeList.after(listingsNav);
 
 				// check for preferred store id, highlight, move to top
 				if (currentTemplate === 'cart'){
@@ -139,7 +143,7 @@ var storeinventory = {
 				}
 				listings.find('.store-' + User.storeId).addClass('selected').find('.select-store-button ').text(selectedButtonText);
 
-				that.bubbleStoreUp(listings, User.storeId);
+				self.bubbleStoreUp(listings, User.storeId);
 
 				// if there is a block to show results on page (pdp)
 				if (currentTemplate !== 'cart') {
@@ -166,35 +170,45 @@ var storeinventory = {
 				}
 
 				// update panel with new list
-				listings.width(numListings * storeTileWidth).appendTo($storeList);
+				listings.width(numListings * storeTileWidth).appendTo(self.$storeList);
 
 				// set up 'set preferred store' action on new elements
-				listings.find('button.select-store-button').click(function(e){
-					var selectedStoreId = $(this).val();
+				// @TODO this needs to be refactored
+				listings.find('button.select-store-button').on('click', function (e) {
+					var $this = $(this);
+					var selectedStoreId = $this.val();
 					if (currentTemplate === 'cart') {
 						//update selected store and set the lineitem
-						var liuuid = $('#preferred-store-panel').find('.srcitem').attr('value');
-						$('div[name="'+liuuid+'-sp"] .selected-store-address').html($(this).siblings('.store-tile-address').text()+' <br />'+jQuery(this).siblings('.store-tile-city').text()+' , '+jQuery(this).siblings('.store-tile-state').text()+' '+jQuery(this).siblings('.store-tile-postalCode').text());
-						$('div[name="'+liuuid+'-sp"] .storeid').val($(this).val());
-						$('div[name="'+liuuid+'-sp"] .selected-store-availability').html($(this).siblings('.store-tile-status'));
-						$('div[name="'+liuuid+'-sp"] .radio-url').removeAttr('disabled');
-						$('div[name="'+liuuid+'-sp"] .radio-url').click();
-						$('#preferred-store-panel').dialog("close");
+						var liuuid = self.$preferredStorePanel.find('.srcitem').attr('value');
+						$('div[name="' + liuuid + '-sp"] .selected-store-address').html(
+							$this.siblings('.store-tile-address').text()
+							+ ' <br />'
+							+ $this.siblings('.store-tile-city').text()
+							+ ' , '
+							+ $this.siblings('.store-tile-state').text()
+							+ ' '
+							+$this.siblings('.store-tile-postalCode').text()
+						);
+						$('div[name="' + liuuid + '-sp"] .storeid').val($this.val());
+						$('div[name="' + liuuid + '-sp"] .selected-store-availability').html($this.siblings('.store-tile-status'));
+						$('div[name="' + liuuid + '-sp"] .radio-url').removeAttr('disabled');
+						$('div[name="' + liuuid + '-sp"] .radio-url').click();
+						self.$preferredStorePanel.dialog("close");
 					} else {
 						if (User.storeId !== selectedStoreId) {
 							// set as selected
-							app.storeinventory.setPreferredStore(selectedStoreId);
-							app.storeinventory.bubbleStoreUp (onPageList, selectedStoreId);
+							self.setPreferredStore(selectedStoreId);
+							self.bubbleStoreUp (onPageList, selectedStoreId);
 							$('.store-list li.selected').removeClass('selected').find('.select-store-button').text(Resources.SELECT_STORE);
 							$('.store-list li.store-' + selectedStoreId + ' .select-store-button').text(Resources.PREFERRED_STORE).parent().addClass('selected');
 						}
 					}
 					//if there is a dialog box open in the cart for editing a pli and the user selected a new store
 					//add an event to for a page refresh on the cart page if the update button has not been clicked
-					//reason - the pli has been updated but the update button was not clicked, leaving the cart visually in accurate.  
+					//reason - the pli has been updated but the update button was not clicked, leaving the cart visually in accurate.
 					//when the update button is clicked it forces a refresh.
 					if ($('#cart-table').length > 0 && $('.select-store-button').length > 0) {
-						$('.ui-dialog .ui-icon-closethick:first').bind( "click", function(){
+						$('.ui-dialog .ui-icon-closethick:first').bind('click', function (){
 							page.refresh();
 						});
 					}
@@ -203,32 +217,34 @@ var storeinventory = {
 		});
 	},
 
-	bubbleStoreUp : function(list, id) {
+	bubbleStoreUp: function(list, id) {
 		var preferredEntry = list.find('li.store-' + id).clone();
 		preferredEntry.removeClass('extended-list');
 		list.find('.store-tile').not('extended-list').addClass('extended-list');
-		list.find('li.store-' + id).remove();
+		list.find('.store-' + id).remove();
 		list.prepend(preferredEntry);
 	},
 
-	loadPreferredStorePanel : function(pid) {
-		var that = this,
-			$preferredStorePanel = $('#preferred-store-panel');
-	//clear error messages from other product tiles if they exists in the dom
-		if($('#preferred-store-panel div .error-message').length > 0){
-			$('#preferred-store-panel div .error-message').remove();
-		}
+	loadPreferredStorePanel: function(pid) {
+		var self = this;
+		//clear error messages from other product tiles if they exists in the dom
+		this.$preferredStorePanel.find('.error-message').remove();
+
 		// clear any previous results
-		$preferredStorePanel.empty();
+		this.$preferredStorePanel.empty();
 
 		// show form if no zip set
-		if (User.zip === null || User.zip === "") {
-			$preferredStorePanel.append('<div><input type="text" id="userZip" class="entered-zip" placeholder="' + Resources.ENTER_ZIP + '"/><button id="set-user-zip" class="button-style-1">' + Resources.SEARCH + '</button></div>')
+		if (User.zip === null || User.zip === '') {
+			this.$preferredStorePanel.append('<div><input type="text" id="userZip" class="entered-zip" placeholder="'
+					+ Resources.ENTER_ZIP
+					+ '"/><button id="set-user-zip" class="button-style-1">'
+					+ Resources.SEARCH
+					+ '</button></div>')
 				.find('#set-user-zip').on('click', function () {
 					var enteredZip = $('.ui-dialog #preferred-store-panel input.entered-zip').last().val();
 					var regexObj = {
-						canada : /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i,
-						usa : /^\d{5}(-\d{4})?$/
+						canada: /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i,
+						usa: /^\d{5}(-\d{4})?$/
 					};
 					var validZipEntry = false;
 
@@ -244,12 +260,12 @@ var storeinventory = {
 					}
 					//good zip
 					if (validZipEntry) {
-						$('#preferred-store-panel div .error-message').remove();
-						that.setUserZip(enteredZip);
-						that.loadPreferredStorePanel(pid);
+						$('#preferred-store-panel .error-message').remove();
+						self.setUserZip(enteredZip);
+						self.loadPreferredStorePanel(pid);
 					//bad zip
 					} else {
-						if ($('#preferred-store-panel div .error-message').length == 0){
+						if ($('#preferred-store-panel .error-message').length == 0){
 							$('#preferred-store-panel div').append('<div class="error-message">' + Resources.INVALID_ZIP + '</div>');
 						}
 					}
@@ -267,75 +283,78 @@ var storeinventory = {
 
 		// zip is set, build list
 		} else {
-			app.storeinventory.buildStoreList(pid);
-			$preferredStorePanel
-				.append("<div>For " + User.zip + " <span class='update-location'>" + Resources.CHANGE_LOCATION + "</span></div>" )
-				.append($('.store-list'));
-			$preferredStorePanel.find('.update-location').on('click', function () {
-				that.setUserZip(null);
-				that.loadPreferredStorePanel(pid);
-			});
+			this.buildStoreList(pid);
+			this.$preferredStorePanel
+				.append('<div>For ' + User.zip + ' <span class="update-location">' + Resources.CHANGE_LOCATION + '</span></div>')
+				.append(this.$storeList);
+			this.$preferredStorePanel.find('.update-location').on('click', function () {
+				this.setUserZip(null);
+				this.loadPreferredStorePanel(pid);
+			}.bind(this));
 		}
 
 		// append close button for pdp
-		if (currentTemplate !== "cart") {
+		if (currentTemplate !== 'cart') {
 			if (User.storeId !== null) {
-				$preferredStorePanel.append("<button class='close button-style-1  set-preferred-store'>" + Resources.CONTINUE_WITH_STORE + "</button>");
+				this.$preferredStorePanel.append('<button class="close button-style-1  set-preferred-store">' + Resources.CONTINUE_WITH_STORE + '</button>');
 			} else if (User.zip !== null) {
-				$preferredStorePanel.append("<button class='close button-style-1'>" + Resources.CONTINUE + "</button>");
+				this.$preferredStorePanel.append('<button class="close button-style-1">' + Resources.CONTINUE + '</button>');
 			}
 		} else {
-			$preferredStorePanel.append("<input type='hidden' class='srcitem' value='" + pid + "'>");
+			this.$preferredStorePanel.append('<input type="hidden" class="srcitem" value="' + pid + '">');
 		}
 
 		// open the dialog
-		$preferredStorePanel.dialog({
+		this.$preferredStorePanel.dialog({
 			width: 550,
+			autoOpen: true,
 			modal: true,
 			title: Resources.STORE_NEAR_YOU
 		});
 
 		// action for close/continue
-		$('.close').click(function(){
-			$preferredStorePanel.dialog("close");
-		});
+		$('.close').on('click', function () {
+			this.$preferredStorePanel.dialog('close');
+		}.bind(this));
 
 		//remove the continue button if selecting a zipcode
-		if (User.zip === null || User.zip === "") {
-			$('#preferred-store-panel .set-preferred-store').last().remove();
+		if (User.zip === null || User.zip === '') {
+			this.$preferredStorePanel.find('.set-preferred-store').last().remove();
 		}
-		
+
 		//disable continue button if a preferred store has not been selected
 		if ($('.store-list .selected').length > 0){
-			$('#preferred-store-panel .close').attr('disabled', false);
+			this.$preferredStorePanel.find('.close').attr('disabled', false);
 		} else {
-			$('#preferred-store-panel .close').attr('disabled', true);
+			this.$preferredStorePanel.find('.close').attr('disabled', true);
 		}
 	},
 
-	setUserZip : function(zip) {
+	setUserZip: function(zip) {
 		User.zip = zip;
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: Urls.setZipCode,
-			data: { zipCode : zip }
+			data: {
+				zipCode: zip
+			}
 		});
 	},
 
-	setPreferredStore : function(id) {
+	setPreferredStore: function(id) {
 		User.storeId = id;
-		$.post(Urls.setPreferredStore, { storeId : id }, function(data) {
+		$.post(Urls.setPreferredStore, {storeId: id}, function(data) {
 			$('.selected-store-availability').html(data);
 			//enable continue button when a preferred store has been selected
 			$('#preferred-store-panel .close').attr('disabled', false);
 		});
 	},
 
-	shippingLoad : function() {
-		var $checkoutForm = $(".address");
-		$checkoutForm.off("click");
-		$checkoutForm.on("click", ".is-gift-yes, .is-gift-no", function (e) {
-			$(this).parent().siblings(".gift-message-text").toggle($(this).checked);
+	shippingLoad: function() {
+		var $checkoutForm = $('.address');
+		$checkoutForm.off('click');
+		$checkoutForm.on('click', '.is-gift-yes, .is-gift-no', function (e) {
+			$(this).parent().siblings('.gift-message-text').toggle($(this).checked);
 		});
 	}
 };
