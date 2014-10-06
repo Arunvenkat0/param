e486e16bd3362f7f1cec9367ba262df8ce88ac6e
Tri Nguyen <tri@tridnguyen.com>
2014-10-06 16:05:07 -0400

    pull in more changes from RAP-2924

--- app_storefront_richUI/cartridge/js/pages/checkout/formPrepare.js
+++ app_storefront_richUI/cartridge/js/pages/checkout/formPrepare.js
@@ -1,50 +1,57 @@
 'use strict';
 
-/**
- * @function
- * @description disable continue button on the page if required inputs are not filled
- * @input String the selector string of the continue button
- * @input String the selector string for the form
- */
-exports.init = function (continueSelector, formSelector) {
-	var $continue = $(continueSelector);
-	var $form = $(formSelector);
-	var validator = $form.validate();
-	var $requiredInputs = $('.required', $form).find(':input');
-	// check for required input
-	function hasEmptyRequired() {
-		// filter out only the visible fields - this allows the checking to work on
-		// billing page where some payment methods inputs are hidden
-		var requiredValues = $requiredInputs.filter(':visible').map(function () {
-			return $(this).val();
-		});
-		return $.inArray('', requiredValues) !== -1;
-	};
+var _ = require('lodash');
 
-	// validate form on init
+var $form, $continue, $requiredInputs, validator;
+
+function init(opts) {
+	if (!opts.formSelector || !opts.continueSelector) {
+		throw new Error('Missing form and continue action selectors.');
+	}
+	$form = $(opts.formSelector);
+	$continue = $(opts.continueSelector);
+	validator = $form.validate();
+	$requiredInputs = $('.required', $form).find(':input');
+	validateForm();
+	// start listening
+	$requiredInputs.on('change', validateEl);
+	$requiredInputs.filter('input').on('keyup', _.debounce(validateEl, 200));
+}
+
+function hasEmptyRequired() {
+	// filter out only the visible fields
+	var requiredValues = $requiredInputs.filter(':visible').map(function () {
+		return $(this).val();
+	});
+	return _(requiredValues).contains('');
+}
+
+function validateForm() {
+	// only validate form when all required fields are filled to avoid
+	// throwing errors on empty form
 	if (!hasEmptyRequired()) {
-		// only validate form when all required fields are filled to avoid
-		// throwing errors on empty form
 		if (validator.form()) {
 			$continue.removeAttr('disabled');
 		}
 	} else {
 		$continue.attr('disabled', 'disabled');
 	}
+}
 
-	function validateInputs() {
-		if ($(this).val() === '') {
-			$continue.attr('disabled', 'disabled');
+function validateEl() {
+	if ($(this).val() === '') {
+		$continue.attr('disabled', 'disabled');
+	} else {
+		// enable continue button on last required field that is valid
+		// only validate single field
+		if (validator.element(this) && !hasEmptyRequired()) {
+			$continue.removeAttr('disabled');
 		} else {
-			// enable continue button on last required field that is valid
-			// only validate single field
-			if (validator.element(this) && !hasEmptyRequired()) {
-				$continue.removeAttr('disabled');
-			} else {
-				$continue.attr('disabled', 'disabled');
-			}
+			$continue.attr('disabled', 'disabled');
 		}
 	}
-
-	$requiredInputs.off('change', validateInputs).on('change', validateInputs);
 }
+
+exports.init = init;
+exports.validateForm = validateForm;
+exports.validateEl = validateEl;
