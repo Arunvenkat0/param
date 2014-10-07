376e34626d43fe8b62f11a88de9ffebefe989bb2
Tri Nguyen <tri@tridnguyen.com>
2014-10-07 16:46:45 -0400

    fix cookie hint popup
    check for privacy policy page

--- app_storefront_richUI/cartridge/js/cookieprivacy.js
+++ app_storefront_richUI/cartridge/js/cookieprivacy.js
@@ -1,18 +1,30 @@
 'use strict';
 
-/** @function cookieprivacy	Used to display/control the scrim containing the cookie privacy code **/
+var dialog = require('./dialog');
 
+/**
+ * @function cookieprivacy	Used to display/control the scrim containing the cookie privacy code
+ **/
 module.exports = function () {
 	/**
-	 * if we have not accepted cookies AND we're not on the Privacy Policy page, then show the notification
-	 *
+	 * If we have not accepted cookies AND we're not on the Privacy Policy page, then show the notification
 	 * NOTE: You will probably want to adjust the Privacy Page test to match your site's specific privacy / cookie page
 	 */
+	 var isPrivacyPolicyPage = $('.content-header').length !== 0 && $('.content-header').text().indexOf('Privacy Policy') !== -1;
 	if (SitePreferences.COOKIE_HINT === true && document.cookie.indexOf('dw_cookies_accepted') < 0) {
-		// Show the Content Asset
-		if ($('h1.content-header').length == 0 || $('h1.content-header')[0].textContent.indexOf('Privacy Policy') < 0) {
-			$('#overlay-background').css('display','block');
-			$('#cookie-hint').css('display','block');
+		if (!isPrivacyPolicyPage) {
+			dialog.open({
+				url: Urls.cookieHint,
+				options: {
+					buttons: {
+						//NOTE: The Close Button handler does the same thing as the 'I Accept" handler - it sets the cookies
+						// and clears the cookie notification.  In a strict situation where you MUST actively accept the cookies
+						// before proceeding, we recommend removing this button from the content asset as well as this handler.
+						'I Agree': buttonHandler,
+						'Close': buttonHandler
+					}
+				}
+			});
 		}
 	} else {
 		// Otherwise, we don't need to show the asset, just enable the cookies
@@ -20,21 +32,11 @@ module.exports = function () {
 	}
 
 	// Close Button handler
-	/**
-	 * NOTE: The Close Button handler does the same thing as the 'I Accept" handler - it sets the cookies
-	 * and clears the cookie notification.  In a strict situation where you MUST actively accept the cookies
-	 * before proceeding, we recommend removing this button from the content asset as well as this handler.
-	 */
-	$('.privacy_close_noaction').on('click', function () {
-		enable_cookies();
-		close_all();
-	});
 
-	// Accept Button handler
-	$('.privacypolicy_agreebtn').on('click', function () {
+	function buttonHandler() {
+		$(this).dialog('close');
 		enable_cookies();
-		close_all();
-	});
+	}
 
 	function enable_cookies() {
 		if (document.cookie.indexOf('dw=1') < 0) {
@@ -44,9 +46,4 @@ module.exports = function () {
 			document.cookie = 'dw_cookies_accepted=1; path=/';
 		}
 	}
-
-	function close_all() {
-		$('#overlay-background').hide();
-		$('#cookie-hint').hide();
-	};
 }
