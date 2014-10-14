419f12beeb1017544392f1b446e97a692ed23367
Tri Nguyen <tri@tridnguyen.com>
2014-10-14 15:38:36 -0400

    add run and webdriver

---
+++ test/xlt/run.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var browserify = require('browserify');
+var remapify = require('remapify');
+var fs = require('fs');
+var b = browserify(__dirname + '/TESTS/SiteGenesis/PUBLIC/smoketest/cart');
+
+var aliasify = require('aliasify').configure({
+	aliases: {
+		'webdriver': './webdriver.js',
+		'globalData': './globalData.json'
+	},
+	configDir: __dirname
+});
+
+b.plugin(remapify, [
+	{
+		src: '**/*.js',
+		expose: 'A_common',
+		cwd: __dirname + '/A_common'
+	}, {
+		src: '**/*.js',
+		expose: 'app',
+		cwd: __dirname + '/app'
+	}
+]);
+
+b.transform(aliasify);
+b.bundle();
