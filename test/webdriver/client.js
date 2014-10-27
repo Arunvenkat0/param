cf03d1455073ade4ae5a37bed8ea4cc35fcda9d3
Tri Nguyen <tri@tridnguyen.com>
2014-10-27 18:52:06 -0400

    rename browser to webdriver

--- test/webdriver/client.js
+++ test/webdriver/client.js
@@ -1,10 +1,21 @@
 var config = require('./config');
+var Promise = require('promise');
 
 var client = require('webdriverio').remote({
 	desiredCapabilities: {
 		browserName: config.client || 'phantomjs'
 	},
 	logLevel: 'silent'
+});
+
+var toPromisify = [
+	'click',
+	'getAttribute',
+	'waitForExist'
+]
+
+toPromisify.forEach(function (m) {
+	client[m] = Promise.denodeify(client[m]).bind(client);
 });
 
 module.exports = client;
