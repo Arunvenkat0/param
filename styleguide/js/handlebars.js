8d0b81d9ea3d949bbdfed27d2bba2539dbb4450b
Tri Nguyen <tri@tridnguyen.com>
2015-01-13 14:55:13 -0500

    import styles from main cartridge

--- styleguide/js/handlebars.js
+++ styleguide/js/handlebars.js
@@ -2,8 +2,6 @@
 
 var Handlebars = require('hbsfy/runtime');
 
-Handlebars.registerPartial('color', require('../templates/color.hbs'));
-
 Handlebars.registerHelper('debug', function (stuff) {
 	console.log(stuff);
 });
