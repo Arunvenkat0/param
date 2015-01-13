24b2da1b6772852d9c0a2fe046215f4d8d93fe7b
Tri Nguyen <tri@tridnguyen.com>
2015-01-13 15:12:35 -0500

    add fonts, simplify model

--- styleguide/js/views/content.js
+++ styleguide/js/views/content.js
@@ -1,17 +1,40 @@
 'use strict';
 
 var View = require('ampersand-view');
-var Collection = require('ampersand-collection');
+var Model = require('ampersand-model');
 var template = require('../../templates/content.hbs');
 var ColorsView = require('./colors');
-var colors = require('../../colors.json');
+var TypographyView = require('./typography');
+
+var TypographyModel = Model.extend({
+	props: {
+		fonts: 'array',
+		headings: {
+			type: 'array',
+			default: function () {return ['h1', 'h2', 'h3', 'h4', 'h5'];}
+		}
+	}
+});
+
+var ColorsModel = Model.extend({
+	props: {
+		colors: 'array'
+	}
+});
 
 var ContentView = View.extend({
 	template: template,
 	render: function () {
 		this.renderWithTemplate();
 		this.renderSubview(new ColorsView({
-			collection: new Collection(colors)
+			model: new ColorsModel({
+				colors: require('../../colors.json')
+			})
+		}));
+		this.renderSubview(new TypographyView({
+			model: new TypographyModel({
+				fonts: require('../../fonts.json')
+			})
 		}));
 		return this;
 	}
