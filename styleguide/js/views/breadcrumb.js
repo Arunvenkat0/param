ef62649f67046fa58f994cdf3986d07f906151d2
Tri Nguyen <tri@tridnguyen.com>
2015-01-15 15:32:46 -0500

    add breadcrumb

---
+++ styleguide/js/views/breadcrumb.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var View = require('ampersand-view');
+var template = require('../../templates/sections/breadcrumb.hbs');
+
+module.exports = View.extend({
+	template: template
+});
--- styleguide/js/views/content.js
+++ styleguide/js/views/content.js
@@ -7,10 +7,12 @@ var template = require('../../templates/content.hbs');
 var ButtonsView = require('./buttons');
 var ColorsView = require('./colors');
 var TypographyView = require('./typography');
+var BreadcrumbView = require('./breadcrumb');
 
 var ButtonsModel = require('../models/buttons');
 var ColorsModel = require('../models/colors');
 var TypographyModel = require('../models/typography');
+var BreadcrumbModel = require('../models/breadcrumb');
 
 var ContentView = View.extend({
 	template: template,
@@ -30,6 +32,11 @@ var ContentView = View.extend({
 			model: new ButtonsModel({
 				button: require('../../templates/elements/button.hbs')(),
 				fancyButton: require('../../templates/elements/buttonFancylarge.hbs')()
+			})
+		}));
+		this.renderSubview(new BreadcrumbView({
+			model: new BreadcrumbModel({
+				demos: [require('../../templates/elements/breadcrumb.hbs')(require('../../breadcrumb.json'))]
 			})
 		}));
 		return this;
