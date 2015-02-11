dff356ca7d4c3038e4b696520de5320b0f321722
Julie Molliver <jmolliver@demandware.com>
2015-02-11 15:43:31 -0500

    product details test for modern blazer

--- test/application/productDetails/index.js
+++ test/application/productDetails/index.js
@@ -1,12 +1,13 @@
 var assert = require('chai').assert;
 var client = require('../webdriver/client');
 var config = require('../webdriver/config');
+var $ = require('jquery');
 
 describe('Product Details Page', function () {
 	before(function (done) {
 		client.init().url(config.url, done);
 	});
-	it('should search for `modern` products', function (done) {
+	it('- Product Details for Modern Blazer', function (done) {
 		client
 			.waitForExist('form[role="search"]')
 			.setValue('#q', 'modern')
@@ -14,13 +15,41 @@ describe('Product Details Page', function () {
 			.waitForExist('#search-result-items', function (err) {
 				assert.equal(err, undefined);
 			})
-			.click('.grid-tile:first-child .name-link', function (err) {
-				assert.equal(err, undefined);
-			})
+			.click('[title$="Modern Blazer"]')
+
 			.getText('h1.product-name', function (err, title) {
 				assert.equal(err, undefined);
-				assert.equal(title, 'Modern Blazer');
+				assert.equal(title, 'Modern Blazer', 'The Product Name should equal Modern Blazer');
 			})
+			.isExisting('.primary-image', function (err, exists) {
+				assert.equal(err, undefined);
+				assert.equal(exists, true, 'The Primary img element should exist');
+			})
+			.isExisting('span.price-sales', function (err, exists) {
+				assert.equal(err, undefined);
+				assert.equal(exists, true, 'The Item Price element should exist');
+			})
+			.getText('#product-content > .product-price .price-sales', function (err, price) {
+				assert.equal(err, undefined);
+				assert.equal(price, '$495.00', 'The price is right');
+			})
+			.isExisting('#Quantity', function (err, exists) {
+					assert.equal(err, undefined);
+					assert.equal(exists, true, 'The Quantity input should exist');
+			})
+			.getAttribute('#Quantity', 'size', function (err, exists) {
+				assert.equal(err, undefined);
+				assert.equal(exists, '2', 'The attribute size is 2');
+			})
+			.getText('#product-content > .product-price .price-sales', function (err, price) {
+				assert.equal(err, undefined);
+				assert.match(price, /^\$.*$/, 'The price should be USD');
+			})
+			.isEnabled('#add-to-cart', function (err, enabled) {
+				assert.equal(err, undefined);
+				assert.notOk(enabled, 'Add to Cart button is disabled');
+			})
+			
 			.call(done);
 	});
 	after(function (done) {
