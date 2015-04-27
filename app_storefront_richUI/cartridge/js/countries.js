dac47a36ec0e48a84d3b0a1278721aca3f775546
Tri Nguyen <tri@tridnguyen.com>
2015-04-27 18:02:14 -0400

    set currency before reload

--- app_storefront_richUI/cartridge/js/countries.js
+++ app_storefront_richUI/cartridge/js/countries.js
@@ -8,10 +8,11 @@ exports.init = function init () {
 		$(this).toggleClass('selector-active');
 		$(this).find('.selector-icon').toggleClass('fa-angle-down fa-angle-up');
 	});
-	$('.country-selector').on('change', function () {
-		var selectedOption = $(this).find('option:selected')[0];
-		var url = selectedOption.getAttribute('data-href');
-		var currency = selectedOption.getAttribute('data-currency');
+	// set currency first before reload
+	$('.country-selector .selector .locale').on('click', function (e) {
+		e.preventDefault();
+		var url = this.href
+		var currency = this.getAttribute('data-currency');
 		$.ajax({
 			dataType: 'json',
 			url: Urls.setSessionCurrency,
