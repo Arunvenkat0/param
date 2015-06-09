77d54dfc56025d97ad301699fa3c8a88576f1efc
Patrick Poon <ppoon@demandware.com>
2015-06-09 11:45:54 -0400

    RAP-3594: Converted indents to spaces

--- test/application/webdriver/pageObjects/testData/inventory.js
+++ test/application/webdriver/pageObjects/testData/inventory.js
@@ -1,17 +1,17 @@
 'use strict';
 
 export function parseInventoryItems (inventoryItems) {
-	let inventoryList = [];
+    let inventoryList = [];
 
-	for (let item of inventoryItems.inventory['inventory-list'][0].records[0].record) {
-		let proxy = {
-			productId: item.$['product-id'],
-			allocation: +item.allocation[0],
-			perpetual: item.perpetual[0] === 'true'
-		};
+    for (let item of inventoryItems.inventory['inventory-list'][0].records[0].record) {
+        let proxy = {
+            productId: item.$['product-id'],
+            allocation: +item.allocation[0],
+            perpetual: item.perpetual[0] === 'true'
+        };
 
-		inventoryList.push(proxy);
-	}
+        inventoryList.push(proxy);
+    }
 
-	return inventoryList;
+    return inventoryList;
 }
