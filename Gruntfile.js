e4813d3f8280eb10f114f97f74d6b18a817781a1
Tri Nguyen <tri@tridnguyen.com>
2015-06-19 15:01:01 -0400

    remove references to richUI

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,7 +127,7 @@ module.exports = function (grunt) {
 				reporter: require('jshint-stylish'),
 				jshintrc: true
 			},
-			target: ['app_storefront_richUI/cartridge/js/**/*.js']
+			target: ['app_storefront_core/cartridge/js/**/*.js']
 		},
 		mochaTest: {
 			application: {
