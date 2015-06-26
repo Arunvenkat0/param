8c963376fd54df3a1d83f3ff72404b942d049427
Evan Chessman <echessman@demandware.com>
2015-06-26 09:36:01 -0400

    [RAP-3130: testing the second portion of the url]

--- test/application/wishList/main.js
+++ test/application/wishList/main.js
@@ -13,30 +13,36 @@ describe('Wishlist', () => {
 	let socialLinksMap = {
 		facebook: {
 			selector: 'a[data-share=facebook]',
-			baseUrl: 'https://www.facebook.com/sharer/sharer.php'
+			baseUrl: 'https://www.facebook.com/sharer/sharer.php',
+			regex: /.*\?.*u=.+/
 		},
 		twitter: {
 			selector: 'a[data-share=twitter]',
-			baseUrl: 'https://twitter.com/intent/tweet/'
+			baseUrl: 'https://twitter.com/intent/tweet/',
+			regex: /.*\?.*url=.+/
 		},
 		googlePlus: {
 			selector: 'a[data-share=googleplus]',
-			baseUrl: 'https://plus.google.com/share'
+			baseUrl: 'https://plus.google.com/share',
+			regex: /.*\?.*url=.+/
 		},
 		pinterest: {
 			selector: 'a[data-share=pinterest]',
-			baseUrl: 'https://www.pinterest.com/pin/create/button/'
+			baseUrl: 'https://www.pinterest.com/pin/create/button/',
+			regex: /.*\?.*url=.+/
 		},
 		emailLink: {
 			selector: 'a[data-share=email]',
-			baseUrl: 'mailto:name@email.com'
+			baseUrl: 'mailto:name@email.com',
+			regex: /.*\&.*body=.+/
 		},
 		shareLinkIcon: {
 		 	selector: wishListPage.CSS_SHARE_LINK,
 		},
 		shareLinkUrl: {
 			selector: '.share-link-content a',
-			baseUrl: wishListPage.configUrl
+			baseUrl: wishListPage.configUrl,
+			regex: /.*\?.*ID=.+/
 		}
 
 	};
@@ -55,35 +61,50 @@ describe('Wishlist', () => {
 			client.isExisting(socialLinksMap.facebook.selector)
 				.then(doesExist => assert.isTrue(doesExist))
 				.then(() => client.getAttribute(socialLinksMap.facebook.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.facebook.baseUrl)))
+				.then(href => {
+					assert.isTrue(href.startsWith(socialLinksMap.facebook.baseUrl))
+					assert.ok(href.match(socialLinksMap.facebook.regex))
+				})
 		);
 
 		it('should display a Twitter icon and link', () => 
 			client.isExisting(socialLinksMap.twitter.selector)
 				.then(doesExist => assert.isTrue(doesExist))
 				.then(() => client.getAttribute(socialLinksMap.twitter.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.twitter.baseUrl)))
+				.then(href => {
+					assert.isTrue(href.startsWith(socialLinksMap.twitter.baseUrl))
+					assert.ok(href.match(socialLinksMap.twitter.regex))
+				})
 		);
 
 		it('should display a Google Plus icon and link', () => 
 			client.isExisting(socialLinksMap.googlePlus.selector)
 				.then(doesExist => assert.isTrue(doesExist))
 				.then(() => client.getAttribute(socialLinksMap.googlePlus.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.googlePlus.baseUrl)))
+				.then(href => {
+					assert.isTrue(href.startsWith(socialLinksMap.googlePlus.baseUrl))
+					assert.ok(href.match(socialLinksMap.googlePlus.regex))
+				})
 		);
 
 		it('should display a Pinterest icon and link', () => 
 			client.isExisting(socialLinksMap.pinterest.selector)
 				.then(doesExist => assert.isTrue(doesExist))
 				.then(() => client.getAttribute(socialLinksMap.pinterest.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.pinterest.baseUrl)))
+				.then(href => {
+					assert.isTrue(href.startsWith(socialLinksMap.pinterest.baseUrl))
+					assert.ok(href.match(socialLinksMap.pinterest.regex))
+				})
 		);
 
 		it('should display a Mail icon and link', () => 
 			client.isExisting(socialLinksMap.emailLink.selector)
 				.then(doesExist => assert.isTrue(doesExist))
 				.then(() => client.getAttribute(socialLinksMap.emailLink.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.emailLink.baseUrl)))
+				.then(href => {
+					assert.isTrue(href.startsWith(socialLinksMap.emailLink.baseUrl))
+					assert.ok(href.match(socialLinksMap.emailLink.regex))
+				})
 		);
 
 		it('should display a link icon', () => 
@@ -97,7 +118,10 @@ describe('Wishlist', () => {
 				.then(() => client.isVisible(socialLinksMap.shareLinkUrl.selector))
 				.then(visible => assert.isTrue(visible))
 				.then(() => client.getAttribute(socialLinksMap.shareLinkUrl.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.shareLinkUrl.baseUrl)))
+				.then(href => {
+					assert.isTrue(href.startsWith(socialLinksMap.shareLinkUrl.baseUrl))
+					assert.ok(href.match(socialLinksMap.shareLinkUrl.regex))
+				})
 		);
 	});
 
