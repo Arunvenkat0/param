dcf14220893c558bd6d1e20fa4e337edd451a236
Evan Chessman <echessman@demandware.com>
2015-06-26 10:37:13 -0400

    clean up conflicts

--- test/application/wishList/main.js
+++ test/application/wishList/main.js
@@ -10,28 +10,6 @@ import * as loginForm from '../webdriver/pageObjects/forms/login';
 describe('Wishlist', () => {
 	let customer;
 	let login = 'testuser1@demandware.com';
-<<<<<<< HEAD
-	let socialLinksMap = {
-		facebook: {
-			selector: 'a[data-share=facebook]',
-			baseUrl: 'https://www.facebook.com/sharer/sharer.php'
-		},
-		twitter: {
-			selector: 'a[data-share=twitter]',
-			baseUrl: 'https://twitter.com/intent/tweet/'
-		},
-		googlePlus: {
-			selector: 'a[data-share=googleplus]',
-			baseUrl: 'https://plus.google.com/share'
-		},
-		pinterest: {
-			selector: 'a[data-share=pinterest]',
-			baseUrl: 'https://www.pinterest.com/pin/create/button/'
-		},
-		emailLink: {
-			selector: 'a[data-share=email]',
-			baseUrl: 'mailto:name@email.com'
-=======
 	let socialLinks = {
 		facebook: {
 			selector: 'a[data-share=facebook]',
@@ -57,22 +35,15 @@ describe('Wishlist', () => {
 			selector: 'a[data-share=email]',
 			baseUrl: 'mailto:name@email.com',
 			regex: /.*\&.*body=.+/
->>>>>>> master
 		},
 		shareLinkIcon: {
 		 	selector: wishListPage.CSS_SHARE_LINK,
 		},
 		shareLinkUrl: {
-<<<<<<< HEAD
-			selector: '.share-link-content.active a',
-			baseUrl: wishListPage.configUrl
-=======
 			selector: '.share-link-content a',
 			baseUrl: wishListPage.configUrl,
 			regex: /.*\?.*WishListID=.+/
->>>>>>> master
 		}
-
 	};
 
 	before(() => client.init());
@@ -81,58 +52,6 @@ describe('Wishlist', () => {
 
 	describe('Send to Friend Links', () => {
 
-<<<<<<< HEAD
-		before(() => wishListPage.navigateTo());
-
-		before(() => loginForm.loginAsDefaultCustomer());
-
-		it('should display a Facebook icon and link', () => 
-			client.isExisting(socialLinksMap.facebook.selector)
-				.then(doesExist => assert.isTrue(doesExist))
-				.then(() => client.getAttribute(socialLinksMap.facebook.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.facebook.baseUrl)))
-		);
-
-		it('should display a Twitter icon and link', () => 
-			client.isExisting(socialLinksMap.twitter.selector)
-				.then(doesExist => assert.isTrue(doesExist))
-				.then(() => client.getAttribute(socialLinksMap.twitter.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.twitter.baseUrl)))
-		);
-
-		it('should display a Google Plus icon and link', () => 
-			client.isExisting(socialLinksMap.googlePlus.selector)
-				.then(doesExist => assert.isTrue(doesExist))
-				.then(() => client.getAttribute(socialLinksMap.googlePlus.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.googlePlus.baseUrl)))
-		);
-
-		it('should display a Pinterest icon and link', () => 
-			client.isExisting(socialLinksMap.pinterest.selector)
-				.then(doesExist => assert.isTrue(doesExist))
-				.then(() => client.getAttribute(socialLinksMap.pinterest.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.pinterest.baseUrl)))
-		);
-
-		it('should display a Mail icon and link', () => 
-			client.isExisting(socialLinksMap.emailLink.selector)
-				.then(doesExist => assert.isTrue(doesExist))
-				.then(() => client.getAttribute(socialLinksMap.emailLink.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.emailLink.baseUrl)))
-		);
-
-		it('should display a link icon', () => 
-			client.isExisting(socialLinksMap.shareLinkIcon.selector)
-				.then(doesExist => assert.isTrue(doesExist))
-		);
-
-		it('should display a link', () => 
-			wishListPage.clickLinkIcon()
-				.then(() => client.isExisting(socialLinksMap.shareLinkUrl.selector))
-				.then(doesExist => assert.isTrue(doesExist))
-				.then(() => client.getAttribute(socialLinksMap.shareLinkUrl.selector, 'href'))
-				.then(href => assert.isTrue(href.startsWith(socialLinksMap.shareLinkUrl.baseUrl)))
-=======
 		before(() => wishListPage.navigateTo()
 			.then(() => loginForm.loginAsDefaultCustomer())
 		);
@@ -202,9 +121,7 @@ describe('Wishlist', () => {
 					assert.isTrue(href.startsWith(socialLinks.shareLinkUrl.baseUrl))
 					assert.ok(href.match(socialLinks.shareLinkUrl.regex))
 				})
->>>>>>> master
 		);
 	});
-
 });
 
