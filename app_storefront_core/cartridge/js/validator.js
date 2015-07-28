4bf61fad4ca19f3b6beacef07bd7e31d4e48cdae
Tri Nguyen <tri@tridnguyen.com>
2015-07-28 17:07:52 -0400

    RAP-3953: localize validation messages

--- app_storefront_core/cartridge/js/validator.js
+++ app_storefront_core/cartridge/js/validator.js
@@ -1,39 +1,38 @@
 'use strict';
 
-var naPhone = /^\(?([2-9][0-8][0-9])\)?[\-\. ]?([2-9][0-9]{2})[\-\. ]?([0-9]{4})(\s*x[0-9]+)?$/,
-	regex = {
-		phone: {
-			us: naPhone,
-			ca: naPhone,
-			fr: /^0[1-6]{1}(([0-9]{2}){4})|((\s[0-9]{2}){4})|((-[0-9]{2}){4})$/,
-			it: /^(([0-9]{2,4})([-\s\/]{0,1})([0-9]{4,8}))?$/,
-			jp: /^(0\d{1,4}- ?)?\d{1,4}-\d{4}$/,
-			cn: /.*/,
-			gb: /^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/
-		},
-		postal: {
-			us: /^\d{5}(-\d{4})?$/,
-			ca: /^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/,
-			fr: /^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$/,
-			it: /^(V-|I-)?[0-9]{4}$/,
-			jp: /^([0-9]){3}[-]([0-9]){4}$/,
-			cn: /^([0-9]){6}$/,
-			gb: /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/
-		},
-		email: /^[\w.%+\-]+@[\w.\-]+\.[\w]{2,6}$/,
-		notCC: /^(?!(([0-9 -]){13,19})).*$/
+var naPhone = /^\(?([2-9][0-8][0-9])\)?[\-\. ]?([2-9][0-9]{2})[\-\. ]?([0-9]{4})(\s*x[0-9]+)?$/;
+var regex = {
+	phone: {
+		us: naPhone,
+		ca: naPhone,
+		fr: /^0[1-6]{1}(([0-9]{2}){4})|((\s[0-9]{2}){4})|((-[0-9]{2}){4})$/,
+		it: /^(([0-9]{2,4})([-\s\/]{0,1})([0-9]{4,8}))?$/,
+		jp: /^(0\d{1,4}- ?)?\d{1,4}-\d{4}$/,
+		cn: /.*/,
+		gb: /^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/
 	},
-	settings = {
-		// global form validator settings
-		errorClass: 'error',
-		errorElement: 'span',
-		onkeyup: false,
-		onfocusout: function (element) {
-			if (!this.checkable(element)) {
-				this.element(element);
-			}
+	postal: {
+		us: /^\d{5}(-\d{4})?$/,
+		ca: /^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/,
+		fr: /^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$/,
+		it: /^(V-|I-)?[0-9]{4}$/,
+		jp: /^([0-9]){3}[-]([0-9]){4}$/,
+		cn: /^([0-9]){6}$/,
+		gb: /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/
+	},
+	notCC: /^(?!(([0-9 -]){13,19})).*$/
+};
+// global form validator settings
+var settings = {
+	errorClass: 'error',
+	errorElement: 'span',
+	onkeyup: false,
+	onfocusout: function (element) {
+		if (!this.checkable(element)) {
+			this.element(element);
 		}
-	};
+	}
+};
 /**
  * @function
  * @description Validates a given phone number against the countries phone regex
@@ -50,17 +49,6 @@ var validatePhone = function (value, el) {
 	var isOptional = this.optional(el);
 	var isValid = rgx.test($.trim(value));
 
-	return isOptional || isValid;
-};
-/**
- * @function
- * @description Validates a given email
- * @param {String} value The email which will be validated
- * @param {String} el The input field
- */
-var validateEmail = function (value, el) {
-	var isOptional = this.optional(el);
-	var isValid = regex.email.test($.trim(value));
 	return isOptional || isValid;
 };
 
@@ -80,12 +68,6 @@ var validateOwner = function (value) {
  * Text fields must have 'phone' css class to be validated as phone
  */
 $.validator.addMethod('phone', validatePhone, Resources.INVALID_PHONE);
-
-/**
- * Add email validation method to jQuery validation plugin.
- * Text fields must have 'email' css class to be validated as email
- */
-$.validator.addMethod('email', validateEmail, Resources.INVALID_EMAIL);
 
 /**
  * Add CCOwner validation method to jQuery validation plugin.
@@ -111,6 +93,25 @@ $.validator.addMethod('positivenumber', function (value) {
 	if ($.trim(value).length === 0) { return true; }
 	return (!isNaN(value) && Number(value) >= 0);
 }, ''); // '' should be replaced with error message if needed
+
+$.extend($.validator.messages, {
+	required: Resources.VALIDATE_REQUIRED,
+	remote: Resources.VALIDATE_REMOTE,
+	email: Resources.VALIDATE_EMAIL,
+	url: Resources.VALIDATE_URL,
+	date: Resources.VALIDATE_DATE,
+	dateISO: Resources.VALIDATE_DATEISO,
+	number: Resources.VALIDATE_NUMBER,
+	digits: Resources.VALIDATE_DIGITS,
+	creditcard: Resources.VALIDATE_CREDITCARD,
+	equalTo: Resources.VALIDATE_EQUALTO,
+	maxlength: $.validator.format(Resources.VALIDATE_MAXLENGTH),
+	minlength: $.validator.format(Resources.VALIDATE_MINLENGTH),
+	rangelength: $.validator.format(Resources.VALIDATE_RANGELENGTH),
+	range: $.validator.format(Resources.VALIDATE_RANGE),
+	max: $.validator.format(Resources.VALIDATE_MAX),
+	min: $.validator.format(Resources.VALIDATE_MIN)
+});
 
 var validator = {
 	regex: regex,
