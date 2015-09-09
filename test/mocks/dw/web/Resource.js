f2a1f06e4f8a75f8f8982a809e3da828f58ba8be
Tri Nguyen <tri@tridnguyen.com>
2015-09-09 15:15:55 -0400

    RAP-4166: fully mock out dw/web/Resource methods
    - Resource.msg
    - Resource.msgf

--- test/mocks/dw/web/Resource.js
+++ test/mocks/dw/web/Resource.js
@@ -1,16 +1,43 @@
 'use strict';
 
-import _ from 'lodash';
+import path from 'path';
+import properties from 'properties-parser';
 
-function msg (val) {
-	return val;
+function msg (key, bundleName, defaultValue) {
+	let bundlePath, props, value;
+	if (!key) {
+		return;
+	}
+	if (bundleName) {
+		bundlePath = path.resolve('./app_storefront_core/cartridge/templates/resources/' + bundleName + '.properties');
+		props = properties.read(bundlePath);
+		value = props[key];
+	}
+	if (!value) {
+		if (defaultValue) {
+			return defaultValue;
+		} else {
+			return key;
+		}
+	}
+	return value;
 }
 function msgf () {
-	return _.chain(arguments)
-		.values()
-		.compact()
-		.value()
-		.join(' ');
+	// pass through to msg if there are no extra format arguments
+	if (arguments.length < 4) {
+		return msg.apply(null, arguments);
+	}
+	let args = Array.prototype.slice.call(arguments);
+	let value = msg.apply(null, args.slice(0, 3));
+	return value.replace(/{(\d)}/g, function (match, p) {
+		let position = Number(p);
+		if (args[position + 3]) {
+			return args[position + 3];
+		// if no arguments found, return the original placeholder
+		} else {
+			return match;
+		}
+	});
 }
 
 module.exports = {
