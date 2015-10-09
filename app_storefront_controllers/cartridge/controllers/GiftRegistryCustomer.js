9c8f98c353b34b01e7a4bf2f2424e498032d0054
Alex Clark <aclark@demandware.com>
2015-10-08 21:33:26 -0400

    [RAP-4311 JSLint: controllers/GiftRegistryCustomers]

--- app_storefront_controllers/cartridge/controllers/GiftRegistryCustomer.js
+++ app_storefront_controllers/cartridge/controllers/GiftRegistryCustomer.js
@@ -5,7 +5,7 @@
  * @module controllers/GiftRegistryCustomer
  * @todo  Requires cleanup
  */
- 
+
 /* API Includes */
 var ISML = require('dw/template/ISML');
 var Pipelet = require('dw/system/Pipelet');
@@ -15,8 +15,7 @@ var g = require('~/cartridges/scripts/guard');
 /**
  * TODO
  */
-function Show()
-{
+function Show() {
     var CurrentHttpParameterMap = request.httpParameterMap;
     var CurrentForms = session.forms;
 
@@ -28,87 +27,76 @@ function Show()
     var ProductList = null;
     var Status = null;
 
-    if (typeof(ProductListID) != 'undefined' && ProductListID != null)
-    {
+    if (typeof(ProductListID) !== 'undefined' && ProductListID !== null) {
         var GetProductListResult = new Pipelet('GetProductList', {
             Create: false
         }).execute({
             ProductListID: ProductListID
         });
-        if (GetProductListResult.result == PIPELET_ERROR)
-        {
+        if (GetProductListResult.result === PIPELET_ERROR) {
             Status = new dw.system.Status(dw.system.Status.ERROR, 'notfound');
-        }
-        else
-        {
+        } else {
             ProductList = GetProductListResult.ProductList;
 
-            if (ProductList.public)
-            {
+            if (ProductList.public) {
                 CurrentForms.giftregistry.items.copyFrom(ProductList.publicItems);
                 CurrentForms.giftregistry.event.copyFrom(ProductList);
-            }
-            else
-            {
+            } else {
                 Status = new dw.system.Status(dw.system.Status.ERROR, 'private');
                 ProductList = null;
             }
         }
-    }
-    else
-    {
+    } else {
         Status = new dw.system.Status(dw.system.Status.ERROR, 'notfound');
     }
 
 
     ISML.renderTemplate('', {
-    	ProductList: ProductList,
-    	Status: Status
+        ProductList: ProductList,
+        Status: Status
     });
 }
 /**
  * TODO
  */
 // TODO fix form in template?
-function ShowInteraction()
-{
-	var TriggeredAction = request.triggeredFormAction;
-	if (TriggeredAction != null)
-	{
-	    if (TriggeredAction.formId == 'purchaseGiftCertificate')
-	    {
-	        var ProductListItem = TriggeredAction.object;
+function ShowInteraction() {
+    var TriggeredAction = request.triggeredFormAction;
+    if (TriggeredAction !== null) {
+        if (TriggeredAction.formId === 'purchaseGiftCertificate') {
 
-	        PurchaseGiftCertificate();
-	        return;
-	    }
-	    else if (TriggeredAction.formId == 'search')
-	    {
-	        var GiftRegistryController = require('./GiftRegistry');
-	        GiftRegistryController.Search();
-	        return;
-	    }
-	}
+            new PurchaseGiftCertificate();
+            return;
+        } else if (TriggeredAction.formId === 'search') {
+            var GiftRegistryController = require('./GiftRegistry');
+            GiftRegistryController.Search();
+            return;
+        }
+    }
 
-	// TODO no default action?
+    // TODO no default action?
 }
 
 
 /**
  * Provides action to add a gift certificate to the basket. As shipping address the address of the gift registry owner is used.
  */
-function PurchaseGiftCertificate()
-{
+function PurchaseGiftCertificate() {
     var CartController = require('./Cart');
     var GetBasketResult = CartController.GetBasket();
-    if (!GetBasketResult.error)
-    {
-    	var Basket = GetBasketResult.Basket;
 
-    	// TODO this can't be right, PLI is not passed if this method is called from the web?
-        var ProductListController = require('./ProductList');
-        var EnsureShipmentResult = ProductList.EnsureShipment({
-        	Basket: Basket
+
+    //TODO : Variable assignment
+    var EnsureShipmentResult;
+    var ProductList;
+    var ProductListController;
+    if (!GetBasketResult.error) {
+        var Basket = GetBasketResult.Basket;
+
+        // TODO this can't be right, PLI is not passed if this method is called from the web?
+        ProductListController = require('./ProductList');
+        EnsureShipmentResult = ProductList.EnsureShipment({
+            Basket: Basket
         });
     }
 
