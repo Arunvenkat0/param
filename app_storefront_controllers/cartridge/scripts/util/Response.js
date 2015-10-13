ef914df4ae1e276cf06fbd94d3f1842382115a88
Alex Clark <aclark@demandware.com>
2015-10-13 14:33:51 -0400

    [RAP-4363 JSLint: script/util/Response]

--- app_storefront_controllers/cartridge/scripts/util/Response.js
+++ app_storefront_controllers/cartridge/scripts/util/Response.js
@@ -9,9 +9,9 @@
 /**
  * Transforms the provided object into JSON format and sends it as JSON response to the client.
  */
- exports.renderJSON = function(object) {
+ exports.renderJSON = function (object) {
     response.setContentType('application/json');
- 
+
     let json = JSON.stringify(object);
     response.writer.print(json);
  };
