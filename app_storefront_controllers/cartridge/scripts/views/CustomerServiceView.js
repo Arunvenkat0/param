1398a56fd5490b8c1d18a45ca432015760f42aaa
Tyrin Avery <tavery@demandware.com>
2015-10-23 17:19:21 -0400

    DOC-1452: documentation for views.

--- app_storefront_controllers/cartridge/scripts/views/CustomerServiceView.js
+++ app_storefront_controllers/cartridge/scripts/views/CustomerServiceView.js
@@ -1,12 +1,23 @@
 'use strict';
 
-/** @module views/CustomerServiceView */
+/**
+ * This view updates the customer navigation before rendering the template.
+ * @module views/CustomerServiceView
+ */
+
 var View = require('./View');
 
 /* API Includes */
 var ContentMgr = require('dw/content/ContentMgr');
 var LinkedHashMap = require('dw/util/LinkedHashMap');
-
+/**
+ * Updates the customer navigation information.
+ *
+ * @class views/CustomerServiceView~CustomerServiceView
+ * @extends module:views/View
+ * @lends module:views/CustomerServiceView~CustomerServiceView.prototype
+ * @returns {module:views/CustomerServiceView~CustomerServiceView} A view with updated information.
+ */
 var CustomerServiceView = View.extend(
 /** @lends module:views/CustomerServiceView~CustomerServiceView.prototype */
 {
@@ -30,20 +41,23 @@ var CustomerServiceView = View.extend(
             for (var i = 0; i < customerServiceFolders.size(); i++) {
                 var folder = customerServiceFolders[i];
 
-                // get the content assets for the folder
+                // Gets the content assets for the folder.
                 var onlineContent = folder.getOnlineContent();
                 //TODO : look at logic of this line - original line -> onlineContent && customerServiceLinks.put(folder.getDisplayName(), onlineContent);
                 customerServiceLinks.put(folder.getDisplayName(), onlineContent);
             }
 
-            // output the target address
+            // Outputs the target address.
             return customerServiceLinks;
         }
     },
 
     /**
-     * @constructs
+     * Adds customer, session, request, and customer service link information to the view.
+     * @constructs CustomerServiceView~CustomerServiceView
      * @extends module:views/View~View
+     * @param {Object} params The parameters to pass to the template for rendering.
+     * @returns {module:views/CartView~CartView} A CustomerService view.
      */
     init: function (params) {
         this._super(params);
