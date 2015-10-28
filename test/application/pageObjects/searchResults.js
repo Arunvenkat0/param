127603d66ac9921768757d824cbf0cb0c511d596
Evan Chessman <echessman@demandware.com>
2015-10-28 11:39:48 -0400

    [RAP-4419: created functional UI tests for search]

--- test/application/pageObjects/searchResults.js
+++ test/application/pageObjects/searchResults.js
@@ -1,3 +1,9 @@
 'use strict';
 
 export const CATEGORY_BANNER = '.content-slot.slot-grid-header';
+export const BREADCRUMB_RESULT_TEXT = '.breadcrumb-element.breadcrumb-result-text';
+export const PDP_MAIN = '.pdp-main';
+export const SEARCH_FORM = 'form[role*=search]';
+export const NO_HITS = '.no-hits-banner';
+export const NO_HITS_TERM_SUGGEST = '.no-hits-search-term-suggest';
+export const PRODUCTID_TEXT = 'span[itemprop*=productID]';
