18132ae7abe8cdea18b4a5f4cab1086adfbae238
Larry Gelberg <lgelberg@demandware.com>
2015-11-02 16:04:22 -0500

    [RAP-4225 - Added tests for checking out with Discover Card]

---
+++ test/application/checkout/discover.js
@@ -0,0 +1,97 @@
+'use strict';
+
+import _ from 'lodash';
+import {assert} from 'chai';
+
+import client from '../webdriver/client';
+import * as checkoutPage from '../pageObjects/checkout';
+import * as orderConfPage from '../pageObjects/orderConfirmation';
+import * as productDetailPage from '../pageObjects/productDetail';
+import * as testData from '../pageObjects/testData/main';
+
+describe('Checkout with Discover Card', () => {
+    let login = 'testuser1@demandware.com';
+    let shippingData = {};
+    let billingFormData = {};
+    let successfulCheckoutTitle = 'Thank you for your order.';
+
+    before(() =>
+        client.init()
+            .then(() => testData.load())
+            .then(() => testData.getCustomerByLogin(login))
+            .then(customer => {
+                let address = customer.getPreferredAddress();
+
+                shippingData = {
+                    firstName: customer.firstName,
+                    lastName: customer.lastName,
+                    address1: address.address1,
+                    country: address.countryCode,
+                    states_state: address.stateCode,
+                    city: address.city,
+                    postal: address.postalCode,
+                    phone: address.phone,
+                    addressList: address.addressId
+                };
+
+                billingFormData = {
+                    emailAddress: customer.email,
+                    creditCard_owner: customer.firstName + ' ' + customer.lastName,
+                    creditCard_type: testData.creditCard2.cardType,
+                    creditCard_number: testData.creditCard2.number,
+                    creditCard_expiration_year: testData.creditCard2.yearIndex,
+                    creditCard_cvn: testData.creditCard2.cvn
+                };
+            })
+            .then(() => Promise.resolve())
+    );
+
+    after(() => client.end());
+
+    function addProductVariationMasterToCart () {
+        return testData.getProductVariationMaster()
+            .then(productVariationMaster => {
+                let product = new Map();
+                product.set('resourcePath', productVariationMaster.getUrlResourcePath());
+                product.set('colorIndex', 1);
+                product.set('sizeIndex', 2);
+                product.set('widthIndex', 1);
+                return product;
+            })
+            .then(product => productDetailPage.addProductVariationToCart(product));
+    }
+
+    let shippingFormData;
+
+    before(() => addProductVariationMasterToCart()
+        .then(() => {
+            // Set form data without preferred address, since manually
+            // entering form fields as Guest
+            shippingFormData = _.cloneDeep(shippingData);
+            delete shippingFormData.addressList;
+        })
+        .then(() => checkoutPage.navigateTo())
+    );
+
+    it('should verify that Discover is available as a credit card option', () =>
+        checkoutPage.pressBtnCheckoutAsGuest()
+            .then(() => checkoutPage.fillOutShippingForm(shippingFormData))
+            .then(() => checkoutPage.checkUseAsBillingAddress())
+            .then(() => client.click(checkoutPage.BTN_CONTINUE_SHIPPING_SAVE))
+            .then(() => client.isExisting(checkoutPage.DISCOVER_CARD))
+            .then(doesExist => assert.isTrue(doesExist))
+    );
+
+    // Fill in Billing Form
+    it('should allow checking out with the Discover card', () =>
+        checkoutPage.fillOutBillingForm(billingFormData)
+            .then(() => client.waitForExist(checkoutPage.BTN_CONTINUE_BILLING_SAVE))
+            .then(() => client.waitForEnabled(checkoutPage.BTN_CONTINUE_BILLING_SAVE))
+            .then(() => client.click(checkoutPage.BTN_CONTINUE_BILLING_SAVE))
+            .then(() => client.click(checkoutPage.BTN_PLACE_ORDER)
+                .waitForVisible(orderConfPage.ORDER_CONF_DETAILS)
+                .then(() => checkoutPage.getLabelOrderConfirmation())
+                .then(title => assert.equal(title, successfulCheckoutTitle)))
+    );
+
+});
