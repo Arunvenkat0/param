9d8d68ddb2359d41c613e6878c2c97a7be191148
Miklos Aubert <miklos.aubert@itelios.com>
2016-01-08 10:14:59 +0000

    Avoid IndexOutOfBoundsException in ProductUtils.getPricing

--- app_storefront_core/cartridge/scripts/product/ProductUtils.js
+++ app_storefront_core/cartridge/scripts/product/ProductUtils.js
@@ -308,7 +308,7 @@ ProductUtils.getPricing = function (item) {
     var isPromoPrice = false;
 
     var promos = PromotionMgr.activeCustomerPromotions.getProductPromotions(item);
-    if (promos) {
+    if (!empty(promos)) {
         var promo = promos[0];
         var promoClass = promo.getPromotionClass();
         if (promoClass && promoClass.equals(Promotion.PROMOTION_CLASS_PRODUCT)) {
