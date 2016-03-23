07c156d538da6df448a91c43f96489e7b34def8f
Evan Chessman <echessman@demandware.com>
2016-03-23 14:33:10 -0400

    [RAP-4642: removed productList and more unused code]

--- app_storefront_controllers/cartridge/controllers/GiftRegistryCustomer.js
+++ app_storefront_controllers/cartridge/controllers/GiftRegistryCustomer.js
@@ -6,9 +6,6 @@
  * @todo  Requires cleanup
  */
 
-/* API Includes */
-var URLUtils = require('dw/web/URLUtils');
-
 /* Script Modules */
 var app = require('~/cartridge/scripts/app');
 var guard = require('~/cartridge/scripts/guard');
@@ -56,8 +53,7 @@ function Show() {
 
     app.getView({
         Status: productListStatus,
-        ProductList: productList,
-        ContinueURL: URLUtils.https('GiftRegistry-SubmitForm')
+        ProductList: productList
     }).render('account/giftregistry/registrycustomer');
 
 }
@@ -65,65 +61,20 @@ function Show() {
  * Gift registry customer event handler. Handles the last triggered action based in the formId.
  *
  * If the formId is:
- * - __purchaseGiftCertificate__ - calls the {@link module:controllers/GiftRegistryCustomer~PurchaseGiftCertificate|PurchaseGiftCertificates} function
- * to add a new gift certificate to the basket.
  * - __search__ - calls the {@link module:controllers/GiftRegistry~search|GiftRegistry controller search function} to render the gift registry search page.
  */
-// TODO fix form in template? Use gift cert functionality instead of this?
 function ShowInteraction() {
-    var TriggeredAction = request.triggeredFormAction;
-    if (TriggeredAction !== null) {
-        if (TriggeredAction.formId === 'purchaseGiftCertificate') {
-
-            new PurchaseGiftCertificate();
-            return;
-        } else if (TriggeredAction.formId === 'search') {
+    app.getForm('giftregistry').handleAction({
+        search: function () {
             var GiftRegistryController = require('./GiftRegistry');
             GiftRegistryController.Search();
             return;
         }
-    }
-
-    // TODO no default action?
+    });
 }
-
-
-/**
- * Provides action to add a gift certificate to the basket. As shipping address the address of the gift registry owner is used.
- */
-function PurchaseGiftCertificate() {
-    var CartController = require('./Cart');
-    var GetBasketResult = CartController.GetBasket();
-
-
-    //TODO : Variable assignment
-    var ProductList;
-    if (!GetBasketResult.error) {
-        var Basket = GetBasketResult.Basket;
-
-        // TODO this can't be right, PLI is not passed if this method is called from the web?
-        require('./ProductList');
-        ProductList.EnsureShipment({
-            Basket: Basket
-        });
-    }
-
-    var GiftCertController = require('./GiftCert');
-    GiftCertController.Purchase();
-}
-
-
-/*
- * Module exports
- */
 
 /*
  * Web exposed methods
  */
 exports.Show = guard.ensure(['get', 'https'], Show);
 exports.ShowInteraction = guard.ensure(['post', 'https'], ShowInteraction);
-
-/*
- * Local methods
- */
-exports.PurchaseGiftCertificate = guard.ensure(['post', 'https'], PurchaseGiftCertificate);
