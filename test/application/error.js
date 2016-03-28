fe09f070443b1f08d12820eb2a87dcc7110cb8ca
Larry Gelberg <lgelberg@demandware.com>
2016-03-28 13:14:15 -0400

    RAP-4737 incorporated code review comments

--- test/application/error.js
+++ test/application/error.js
@@ -20,6 +20,7 @@ describe('Error Redirects', () => {
             .then(() => browser.submitForm(search.SEARCH_FORM))
             .waitForVisible(common.BREADCRUMB_A)
             .getText(common.BREADCRUMB_A)
+            // TODO: Update to retrieve localized string once RAP-4831 (testData: Create parser for library.xml) has been completed.
             .then(text => assert.equal(text, aboutUsString))
     );
 
@@ -30,6 +31,7 @@ describe('Error Redirects', () => {
             .url()
             .then(currentUrl => {
                 let parsedUrl = url.parse(currentUrl.value);
+                assert.isTrue(parsedUrl.href.startsWith('https'));
                 return assert.isTrue(parsedUrl.pathname.endsWith('contactus'));
             })
     );
