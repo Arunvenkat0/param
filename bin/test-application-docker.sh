0b1ec288c50b20a3d6b6826d61cbe7be7b2c12be
Tri Nguyen <tri@tridnguyen.com>
2016-04-12 02:02:44 -0400

    RAP-4814: remove any pre-existing selenium container

--- bin/test-application-docker.sh
+++ bin/test-application-docker.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 CONTAINER_NAME="sg-application-test-chrome"
+
+# remove any pre-existing container
+if [ $(docker ps -a | grep $CONTAINER_NAME | awk '{print $NF}' | wc -l) -gt 0 ]; then
+    docker rm -f $CONTAINER_NAME 1>/dev/null
+fi
+
 # if a debug flag is passed in, use the debug image and open vnc screen sharing
 if [[ $@ == *"--debug"* ]]; then
     ip=$(grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' <<< "$@")
