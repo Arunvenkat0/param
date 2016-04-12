04710613e47472ec795bd61304ba36e27a1bddf0
Tri Nguyen <tri@tridnguyen.com>
2016-04-12 01:33:34 -0400

    RAP-4184: update selenium to version 2.53.0

--- bin/test-application-docker.sh
+++ bin/test-application-docker.sh
@@ -4,11 +4,11 @@ CONTAINER_NAME="sg-application-test-chrome"
 # if a debug flag is passed in, use the debug image and open vnc screen sharing
 if [[ $@ == *"--debug"* ]]; then
     ip=$(grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' <<< "$@")
-    docker run -d --name $CONTAINER_NAME -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-chrome-debug:2.52.0 1>/dev/null
+    docker run -d --name $CONTAINER_NAME -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-chrome-debug:2.53.0 1>/dev/null
     sleep 2 # wait a bit for container to start
     open vnc://:secret@"$ip":5900
 else
-    docker run -d --name $CONTAINER_NAME -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:2.52.0 1>/dev/null
+    docker run -d --name $CONTAINER_NAME -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:2.53.0 1>/dev/null
 fi
 
 # run actual test command in a subshell to be able to rm docker container afterwards
